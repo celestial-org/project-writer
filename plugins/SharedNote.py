9abef9058e91469541d90026709ca63d128a29d1
mymaking <152869680+mymaking@users.noreply.github.com>
2023-12-29 08:03:48 +0700

    update plugins/SharedNote.py

--- plugins/SharedNote.py
+++ plugins/SharedNote.py
@@ -24,10 +24,12 @@ def add_url(c, m):
       save_url("share", url)
       l += 1
     except Exception as e:
-      err = m.reply_text(f'Error: {e}')
-      time.sleep(5)
-      c.delete_messages(m.chat.id, err.id)
-      m.delete()
+      pass
+  if l != len(urls):
+    x = len(urls) - l 
+    temp = m.reply(f"{x} URL trùng lặp sẽ không được thêm lại")
+    time.sleep(5)
+    c.delete_messages(m.chat.id, temp.id)
   done = m.reply_text(f'Đã thêm {l} URL')
   time.sleep(10)
   c.delete_messages(m.chat.id, done.id)
