eab5fbd3236f28ebc63db2cc35bc7dc7d3afe50f
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-05 22:17:35 +0700

    update main.py

--- main.py
+++ main.py
@@ -8,10 +8,6 @@ bot = Client("writer",
              bot_token=tokens()[2],
              plugins={"root": "plugins"})
 
-@bot.on_message(filters.command(["start", "help"]))
-def send_welcome(c, m):
-  m.reply_text(f"Xin chào {m.from_user.first_name}(`{m.from_user.id}`)\n```Công cụ:\n{config_tool}```")
-  
 bot.start()
 os.system('echo V2Writer')
 idle()
