462d374d155cc0b7993846371840b2095c2d6e61
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-05 11:02:35 +0700

    update database.py

--- database.py
+++ database.py
@@ -55,7 +55,7 @@ def check_all(filename):
   if existing_entry:
     removed_urls = []
     for url in existing_entry['urls']:
-      response = requests.get(f"https://convert.v2ray-subscribe.workers.dev/?url={url}")
+      response = requests.get(url, headers={"User-Agent":"v2rayNG/1.8.12"})
       if response.status_code != 200 or response.text is None:
           removed_urls.append(url)
     updated_urls = [u for u in existing_entry['urls'] if u not in removed_urls]
