87244096f598748d1157db742d02bdf62bb0083b
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-05 13:05:29 +0700

    update plugins/TestConfig.py

--- plugins/TestConfig.py
+++ plugins/TestConfig.py
@@ -64,3 +64,46 @@ def run_lite_command(c, m):
     os.system('rm out.png')
     time.sleep(5)
     stt.delete()
+    
+@Client.on_message(filters.command("test"))
+def run_lite_command(c, m):
+  url_pattern = re.compile(r'((http[s]?|vmess|trojan|vless|ss)://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)')
+  ping = 1
+  if len(m.command) > 1 and m.command[1].isdigit():
+    ping = int(m.command[1])
+  if m.reply_to_message:
+    try:
+      text = m.reply_to_message.text
+    except:
+      try:
+        text = m.reply_to_message.caption
+      except:
+        m.reply("Không tìm thấy tin nhắn văn bản", quote=True)
+        return
+  else:
+    try:
+     text = m.text
+    except:
+      try:
+        text = m.caption
+      except:
+        m.reply("Không tìm thấy tin nhắn văn bản", quote=True)
+        return
+  matches = re.findall(url_pattern, text)
+  urls = [match[0] for match in matches]
+  if not urls:
+    m.reply("Không tìm thấy URL trong tin nhắn văn bản", quote=True)
+    return
+  for url in urls:
+    test_url, count = get_config(url)
+    if count is None:
+      m.reply("Liên kết bị lỗi", quote=True)
+      return
+    stt = m.reply(f'**{m.from_user.first_name}** vừa bắt đầu đợt kiểm tra mới đến liên kết {url}\nSố lượng cấu hình: **{count}**\nSố lần kiểm tra: **{ping}**\n```Lưu ý:\nSố lần kiểm tra cao có thể gây quá tải hệ thống và không trả về kết quả như mong đợi\n```', quote=True)
+    cmd = f"./lite -ping {ping} -config pingonly.json -test {test_url}"
+    os.system(cmd)
+    m.reply_photo(photo='out.png', quote=True, caption=f"```sponsor\nTran Han Thang\n```\n**{m.from_user.first_name}**", reply_markup=InlineKeyboardMarkup(
+    [[InlineKeyboardButton("Subscription", url=url)]]))
+    os.system('rm out.png')
+    time.sleep(5)
+    stt.delete()
