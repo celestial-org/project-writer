568231df5de3729103f8d48758e8a7bb4803f333
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-05 20:46:43 +0700

    update db/endpoints.py

---
+++ db/endpoints.py
@@ -0,0 +1,20 @@
+from db.base import endpoints as db
+
+def add(sponsor, prefix, endpoint):
+  db.put(data={"sponsor": sponsor, "endpoint": endpoint}, key=prefix)
+ 
+def get(prefix):
+  item = get(prefix)
+  sponsor = item["sponsor"]
+  endpoint = item["endpoint"]
+  return sponsor, endpoint
+  
+def list():
+  items = db.fetch().items
+  results = []
+  for item in items:
+    prefix = item["prefix"]
+    sponsor = item["sponsor"]
+    result = f"{prefix} - **{sponsor}**"
+    results.append(result)
+  return results
