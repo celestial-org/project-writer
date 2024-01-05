0d719fb72a7e204d46d6dc98b1a6e1c7fb7f4809
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-05 07:18:28 +0700

    update plugins/TestConfig.py

--- plugins/TestConfig.py
+++ plugins/TestConfig.py
@@ -4,31 +4,46 @@ import time
 import requests
 import base64
 import os
+import re
+
+def get_config(url):
+  if any(url.startswith(sche) for sche in ["vmess://", "trojan://", "vless://", "ss://"]):
+    res = [url]
+    url = requests.post("https://paste.rs/", data=res).text
+  else:
+    res = requests.get(url, headers={"User-Agent": "v2rayNG"})
+    if res.text is None:
+      return None
+    res = res.text
+    if not any(res.startswith(sche) for sche in ["vmess", "trojan", "vless", "ss://"]):
+      res = base64.b64decode(res.encode('utf-8')).decode('utf-8')
+      url = requests.post("https://paste.rs/", data=res).text
+  count = len(res.splitlines())
+  return url, count
 
 @Client.on_message(filters.command(["test"]))
 def run_command(c, m):
-  if len(m.command) > 1:
-    url = m.command[1]
-    count = 1
-    if not any(url.startswith(sche) for sche in ["vmess://", "trojan://", "vless://", "ss://"]):
-      res = requests.get(url, headers={"User-Agent": "v2rayNG"})
-      if res.status_code != 200:
-        res = requests.get("https://convert.v2ray-subscribe.workers.dev", params={"url":url})
-        if res.text is None:
-          m.reply("Liên kết không khả dụng", quote=True)
-          return
-      res = res.text
-      if not any(res.startswith(sche) for sche in ["vmess", "trojan", "vless", "ss://"]):
-        res = base64.b64decode(res.encode('utf-8')).decode('utf-8')
-        url = requests.post("https://paste.rs/", data=res).text
-      count = len(res.splitlines())
+  pattern = re.compile(r'https?://|vmess://|trojan://|vless://|ss://\S+')
+  if m.reply_to_message:
+    if not m.reply_to_message.text:
+      m.reply("Không tìm thấy tin nhắn", quote=True)
+      return
+    text = m.reply_to_message.text
+  else:
+    if not m.text:
+      m.reply("Không tìm thấy tin nhắn", quote=True)
+      return
+  text = m.text
+  urls = re.findall(url_pattern, text)
+  for url in urls:
+    url, count = get_config(urls)
+    if count is None:
+      m.reply("Liên kết bị lỗi", quote=True)
+      return
     stt = m.reply(f'**{m.from_user.first_name}** vừa bắt đầu đợt kiểm tra kiểm tra với **{count}** cấu hình', quote=True)
     cmd = f"./lite -ping 1 -test {url}"
     os.system(cmd)
-    time.sleep(3)
-    m.reply_photo(photo='out.png', quote=True, caption=f"{url} \nby **{m.from_user.first_name}**")
+    m.reply_photo(photo='out.png', quote=True, caption=f"{urls} \nby **{m.from_user.first_name}**```tài trợ bởi Tran Han Thang```")
     os.system('rm out.png')
     time.sleep(5)
     stt.delete()
-  else:
-    m.reply("Vui lòng cung cấp subscribe hoặc cấu hình", quote=True)
