08248867933af8457f4f3a71ea6444b34b7e2755
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-05 15:14:56 +0700

    update plugins/TestConfig.py

--- plugins/TestConfig.py
+++ plugins/TestConfig.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from lib.lite import get_config, local_test
 import subprocess
 import time
 import requests
@@ -7,27 +8,9 @@ import base64
 import os
 import re
 
-def get_config(url):
-  if any(scheme in url for scheme in ["vmess:", "trojan:", "vless:", "ss:"]):
-    res = url
-    url = requests.post("https://paste.rs/", data=url).text
-  else:
-    res = requests.get(url, headers={"User-Agent": "v2rayNG"})
-    if res.text is None:
-      return None
-    res = res.text
-    if not any(res.startswith(sche) for sche in ["vmess", "trojan", "vless", "ss://"]):
-      res = base64.b64decode(res.encode('utf-8')).decode('utf-8')
-      url = requests.post("https://paste.rs/", data=res).text
-  count = len(res.splitlines())
-  return url, count
-
 @Client.on_message(filters.command("test"))
 def run_lite_command(c, m):
   url_pattern = re.compile(r'((http[s]?|vmess|trojan|vless|ss)://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)')
-  ping = 1
-  if len(m.command) > 1 and m.command[1].isdigit():
-    ping = int(m.command[1])
   if m.reply_to_message:
     try:
       text = m.reply_to_message.text
@@ -56,9 +39,8 @@ def run_lite_command(c, m):
     if count is None:
       m.reply("Liên kết bị lỗi", quote=True)
       return
-    stt = m.reply(f'**{m.from_user.first_name}** vừa bắt đầu đợt kiểm tra mới đến liên kết {url}\nSố lượng cấu hình: **{count}**\nSố lần kiểm tra: **{ping}**\n```Lưu ý:\nSố lần kiểm tra cao có thể gây quá tải hệ thống và không trả về kết quả như mong đợi\n```', quote=True)
-    cmd = f"./lite -ping {ping} -config config.json -test {test_url}"
-    os.system(cmd)
+    stt = m.reply(f'**{m.from_user.first_name}** vừa bắt đầu đợt kiểm tra mới đến liên kết {url} với **{count}** cấu hình\n```Lưu ý:\nQuá nhiều cấu hình có thể gây ra lỗi và không trả về kết quả\n```', quote=True)
+    local_test(test_url)
     m.reply_photo(photo='out.png', quote=True, caption=f"```sponsor\nTran Han Thang\n```\n**{m.from_user.first_name}**", reply_markup=InlineKeyboardMarkup(
     [[InlineKeyboardButton("Subscription", url=url)]]))
     os.system('rm out.png')
@@ -68,9 +50,6 @@ def run_lite_command(c, m):
 @Client.on_message(filters.command("ping"))
 def run_lite_command_ping(c, m):
   url_pattern = re.compile(r'((http[s]?|vmess|trojan|vless|ss)://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)')
-  ping = 1
-  if len(m.command) > 1 and m.command[1].isdigit():
-    ping = int(m.command[1])
   if m.reply_to_message:
     try:
       text = m.reply_to_message.text
@@ -99,9 +78,8 @@ def run_lite_command_ping(c, m):
     if count is None:
       m.reply("Liên kết bị lỗi", quote=True)
       return
-    stt = m.reply(f'**{m.from_user.first_name}** vừa bắt đầu đợt kiểm tra mới đến liên kết {url}\nSố lượng cấu hình: **{count}**\nSố lần kiểm tra: **{ping}**\n```Lưu ý:\nSố lần kiểm tra cao có thể gây quá tải hệ thống và không trả về kết quả như mong đợi\n```', quote=True)
-    cmd = f"./lite -ping {ping} -config pingonly.json -test {test_url}"
-    os.system(cmd)
+    stt = m.reply(f'**{m.from_user.first_name}** vừa bắt đầu đợt kiểm tra độ trễ với **{count}** cấu hình từ liên kết {url}\n```Lưu ý:\nQuá nhiều cấu hình có thể gây ra lỗi và không trả về kết quả\n```', quote=True)
+    local_test(test_url, pingonly=True)
     m.reply_photo(photo='out.png', quote=True, caption=f"```sponsor\nTran Han Thang\n```\n**{m.from_user.first_name}**", reply_markup=InlineKeyboardMarkup(
     [[InlineKeyboardButton("Subscription", url=url)]]))
     os.system('rm out.png')
