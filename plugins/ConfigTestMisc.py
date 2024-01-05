cc00fdb13b40dcae0db637083b7276559a639244
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-05 21:26:12 +0700

    update plugins/ConfigTestMisc.py

--- plugins/ConfigTestMisc.py
+++ plugins/ConfigTestMisc.py
@@ -25,8 +25,23 @@ def add_endpoint(c, m):
       m.reply("Chỉ chấp nhận giao thức http/https", quote=True)
       return
     sponsor = m.from_user.first_name
-    ep.add(sponsor, prefix, endpoint)
+    sponsor_id = m.from_user.id
+    ep.add(sponsor, sponsor_id, prefix, endpoint)
     m.reply(f"**{sponsor}** đã đóng góp vào hệ thống một điểm cuối với tiền tố {prefix}. Lệnh /test{prefix} đã có sẵn", quote=True)
   except:
     m.reply("**Lỗi:**\nVui lòng làm theo mẫu để thêm điểm cuối\n```guide\n/addpoint + tiền tố + url điểm cuối\nví dụ: /addpoint vn http://103.0.0.0:80\n```", quote=True)
   
+@Client.on_message(filters.command("killpoint"))
+def remove_endpoint(c, m):
+  if len(m.command) != 2:
+    m.reply("Vui lòng cung cấp tiền tố điểm cuối cần xoá", quote=True)
+    return
+  prefix = m.command[1]
+  uid = m.from_user.id
+  try:
+    _, sid, __ = ep.get(prefix)
+    if uid not in [int(sid), 5665225938]:
+      m.reply(f"**{m.from_user.first_name} ** điểm cuối này không thuộc sở hữu của bạn, không thể hoàn thành thao tác xoá", quote=True)
+      return
+    ep.rm(prefix)
+    m.reply("Đã xoá điểm cuối", quote=True)
