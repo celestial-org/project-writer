4e13e0829ff372ce8a0cde03777dcc32eeeb400e
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-06 21:51:08 +0700

    update plugins/Setup Server.py

--- plugins/Setup Server.py
+++ plugins/Setup Server.py
@@ -46,3 +46,47 @@ def run_server(hostname, username, password, ssh_port, http_port):
         return f"Docker command completed with exit status: {stdout.channel.recv_exit_status()}"
   except Exception as e:
       return f"Error: {e}"
+      
+@Client.on_message(filters.command("runssh"))
+def setup_server(c, m):
+  if len(m.command) < 5:
+    m.reply('Để chạy lệnh ssh vui lòng cung thực hiện theo mẫu:\n\n /runssh h=\"hostname\" u=\"login user\" pw=\"login password\" ssh=\"cổng SSH\" cmd=\"lệnh shell\"', quote=True)
+    return
+  pattern = re.compile(r'(\w+)="([^"]*)"')
+  matches = pattern.findall(m.text)
+  params = dict(matches)
+  hostname = params.get('h')
+  if not hostname:
+    m.reply("hostname là tham số bắt buộc! Vui lòng thực hiện lại", quote=True)
+    return
+  username = params.get('u')
+  if not username:
+    m.reply("username là tham số bắt buộc! Vui lòng thực hiện lại", quote=True)
+    return
+  password = params.get('pw')
+  if not password:
+    m.reply("mật khẩu là tham số bắt buộc! Vui lòng thực hiện lại", quote=True)
+    return
+  ssh_port = params.get('ssh')
+  if not ssh_port:
+    m.reply("cổng kết nối là bắt buộc! Vui lòng thực hiện lại", quote=True)
+    return
+  cmd = params.get('cmd')
+  if not cmd:
+    m.reply("Vui lòng cung cấp lệnh shell", quote=True)
+    return
+  handle = run_cmd(hostname, username, password, ssh_port, http_port)
+  m.reply(handle, quote=True)
+  
+def run_cmd(hostname, username, password, ssh_port, cmd):
+  try:
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname, ssh_port, username, password)
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    while not stdout.channel.exit_status_ready():
+        time.sleep(1)
+        ssh.close()
+        return stdout.read().decode('utf-8')
+  except Exception as e:
+      return f"Error: {e}"
