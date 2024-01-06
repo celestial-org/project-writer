2a065b5e03811b11899e95a974ba6a2f4a79b9b0
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-06 21:44:15 +0700

    update plugins/TestConfig/Local Tester.py

--- plugins/TestConfig/Local Tester.py
+++ plugins/TestConfig/Local Tester.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from lib.lite import get_config, local_test, endpoint_test
+from lib.lite import get_config, endpoint_test
 from db import endpoints as eps
 import subprocess
 import time
@@ -50,71 +50,16 @@ def run_lite_command(c, m):
       stt = m.reply(f'**{m.from_user.first_name}** vừa bắt đầu đợt kiểm tra mới đến liên kết {url} với **{count}** cấu hình', quote=True)
     else:
       stt = m.reply(f'**{m.from_user.first_name}** vừa bắt đầu đợt kiểm tra mới đến 1 cấu hình\n{test_url}', quote=True)
+    if not prefix:
+        prefix == "us"
     try:
-      if not prefix:
-        raise
-      try:
-        sponsor, _, endpoint = eps.get(prefix)
-      except:
-        stt = m.reply("Điểm cuối không khả dụng", quote=True)
-        time.sleep(10)
-        stt.delete()
-        raise
-      photo, city, country, org = endpoint_test(test_url, endpoint)
-      m.reply_photo(photo=photo, quote=True, caption=f"```sponsor\n{sponsor}\n```\n**{city}-{country}\n{org}**\n\n**{m.from_user.first_name}**")
+      sponsor, _, endpoint = eps.get(prefix)
     except:
-      local_test(test_url)
-      m.reply_photo(photo='out.png', quote=True, caption=f"```sponsor\nTran Han Thang\n```\n**{m.from_user.first_name}**")
-      os.system('rm out.png')
-    time.sleep(5)
-    stt.delete()
-    
-@Client.on_message(filters.command("ping"))
-def run_lite_command_ping(c, m):
-  url_pattern = re.compile(r'((http[s]?|vmess|trojan|vless|ss)://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)')
-  if m.reply_to_message:
-    try:
-      text = m.reply_to_message.text
-    except:
-      try:
-        text = m.reply_to_message.caption
-      except:
-        m.reply("Không tìm thấy tin nhắn văn bản", quote=True)
-        return
-  else:
-    try:
-     text = m.text
-    except:
-      try:
-        text = m.caption
-      except:
-        m.reply("Không tìm thấy tin nhắn văn bản", quote=True)
-        return
-  matches = re.findall(url_pattern, text)
-  urls = [match[0] for match in matches]
-  if not urls:
-    m.reply("Không tìm thấy URL trong tin nhắn văn bản", quote=True)
-    return
-  for url in urls:
-    test_url, count = get_config(url)
-    if count is None:
-      m.reply("Liên kết bị lỗi", quote=True)
+      stt = m.reply("Điểm cuối không khả dụng", quote=True)
+      time.sleep(10)
+      stt.delete()
       return
-    stt = m.reply(f'**{m.from_user.first_name}** vừa bắt đầu đợt kiểm tra độ trễ với **{count}** cấu hình từ liên kết {url}\n```Lưu ý:\nQuá nhiều cấu hình có thể gây ra lỗi và không trả về kết quả\n```', quote=True)
-    local_test(test_url, pingonly=True)
-    m.reply_photo(photo='out.png', quote=True, caption=f"```sponsor\nTran Han Thang\n```\n**{m.from_user.first_name}**")
-    os.system('rm out.png')
+    photo, city, country, org = endpoint_test(test_url, endpoint)
+    m.reply_photo(photo=photo, quote=True, caption=f"```sponsor\n{sponsor}\n```\n**{city}-{country}\n{org}**\n\n**{m.from_user.first_name}**")
     time.sleep(5)
     stt.delete()
-    
-@Client.on_message(filters.command('set_endpoint'))
-def set_endpoint(c, m):
-  if len(m.command) < 2:
-    m.reply("Vui lòng cung cấp điểm cuối ```ví dụ\nhttps://hostname\nhttp://--ip--:80\n```", quote=True)
-    return
-  endpoint = m.command[1]
-  os.environ["ENDPOINT"] = endpoint
-  stt = m.reply(f"Đã thiết lập điểm cuối thành {endpoint}", quote=True)
-  time.sleep(10)
-  m.delete()
-  stt.delete()
