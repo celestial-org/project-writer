ba9233f3ddc29d8df7a9d7e3178e0af2fe557ef5
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-06 07:15:21 +0700

    update plugins/TestConfig/Endpoints Manage.py

--- plugins/TestConfig/Endpoints Manage.py
+++ plugins/TestConfig/Endpoints Manage.py
@@ -26,7 +26,7 @@ def list_endpoints(c, m):
   save.save(m.from_user)
   tests = ep.get_list()
   endpoints = "\n".join(tests)
-  echo = f"```hướng dẫn:\nsử dụng lệnh /test với tiền tố của điểm cuối để kiểm tra ở cuối đó\n```\n**Danh sách: (lệnh - người đóng góp)**\n\n"
+  echo = f"```hướng dẫn: /test+prefix```\n**Danh sách:**\n\n"
   text = f"{echo}{endpoints}"
   m.reply(text, quote=True)
   
