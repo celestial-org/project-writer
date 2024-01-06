4ffce4fecc96dde69055b4829735e0679c489731
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-06 21:43:40 +0700

    update plugins/TestConfig/Endpoint Tester.py

--- plugins/TestConfig/Endpoint Tester.py
+++ plugins/TestConfig/Endpoint Tester.py
@@ -1,66 +1,75 @@
 from pyrogram import Client, filters
-from db import endpoints as ep
-from lib.lite import get_config, endpoint_test
 from db import save
-import re 
+from db import endpoints as ep
+import os 
 import time
 
-url_pattern = re.compile(r'((http[s]?|vmess|trojan|vless|ss)://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)')
-
-@Client.on_message(filters.regex("/test"), group=2)
-def regex_lite_command(c, m):
-  if not m.text.startswith("/test"):
-    return
+@Client.on_message(filters.command("setpoint") & filters.user(5665225938))
+def set_local_endpoint(c, m):
   save.save(m.from_user)
-  command = m.text.split(' ')[0]
-  if "@v2writer_bot" in command:
-    command = command.replace("@v2writer_bot", "")
-  prefix = command.replace("/test", "")
   try:
-    sponsor, _, endpoint = ep.get(prefix)
-  except:
-    return
-  if endpoint is None:
-    return
-  if m.reply_to_message:
-    try:
-      text = m.reply_to_message.text
-    except:
-      try:
-        text = m.reply_to_message.caption
-      except:
-        m.reply("Không tìm thấy tin nhắn văn bản", quote=True)
-        return
-  else:
-    try:
-     text = m.text
-    except:
-      try:
-        text = m.caption
-      except:
-        m.reply("Không tìm thấy tin nhắn văn bản", quote=True)
-        return
-  matches = re.findall(url_pattern, text)
-  urls = [match[0] for match in matches]
-  if not urls:
-    m.reply("Không tìm thấy URL trong tin nhắn văn bản", quote=True)
-    return
-  for url in urls:
-    try:
-      test_url, count = get_config(url)
-    except:
-      uvl = m.reply("Liên kết không khả dụng", quote=True)
-      time.sleep(10)
-      uvl.delete()
-      return
-    if count is None:
-      m.reply("Liên kết bị lỗi", quote=True)
-      return
-    if url.startswith("http"):
-      stt = m.reply(f'**{m.from_user.first_name}** vừa bắt đầu đợt kiểm tra mới đến liên kết {url} với **{count}** cấu hình', quote=True)
+    if len(m.command) > 1:
+      ep.get(m.command[1])
+      os.environ["ENDPOINT"] = m.command[1]
+      st = m.reply("Đã chuyển đổi điểm cuối mặc định")
     else:
-      stt = m.reply(f'**{m.from_user.first_name}** vừa bắt đầu đợt kiểm tra mới đến 1 cấu hình\n{test_url}', quote=True)
-    photo, city, country, org = endpoint_test(test_url, endpoint)
-    m.reply_photo(photo=photo, quote=True, caption=f"```sponsor\n{sponsor}\n```\n**{city}-{country}\n{org}**\ntester: **{m.from_user.first_name}**")
-    time.sleep(5)
-    stt.delete()
+      del os.environ["ENDPOINT"]
+      st = m.reply("Đã khôi phục điểm cuối mặc định")
+  except Exception as e:
+    st = m.reply(f"Lỗi: {e}", quote=True)
+  time.sleep(20)
+  st.delete()
+
+@Client.on_message(filters.command("endpoints"))
+def list_endpoints(c, m):
+  save.save(m.from_user)
+  tests = ep.get_list()
+  endpoints = "\n".join(tests)
+  echo = f"```hướng dẫn: /test+prefix```\n**Danh sách:**\n\n"
+  text = f"{echo}{endpoints}"
+  st = m.reply(text, quote=True)
+  time.sleep(60)
+  st.delete()
+  m.delete()
+  
+@Client.on_message(filters.command("addpoint"))
+def add_endpoint(c, m):
+  save.save(m.from_user)
+  try:
+    if len(m.command) < 3:
+      raise Exception("Vui lòng làm theo mẫu để thêm điểm cuối\n```guide\n/addpoint + tiền tố + url điểm cuối\nví dụ: /addpoint vn http://103.0.0.0:80\n```")
+    prefix = m.command[1]
+    if len(prefix) > 5:
+      raise Exception("Tiền tố tối đa là 5 ký tự. Vui lòng thừ lại")
+    endpoint = m.command[2]
+    if not endpoint.startswith("http"):
+      raise Exception("Chỉ chấp nhận giao thức http/https")
+      return
+    sponsor = m.from_user.first_name
+    sponsor_id = m.from_user.id
+    ep.add(sponsor, sponsor_id, prefix, endpoint)
+    st = m.reply(f"**{sponsor}** đã đóng góp vào hệ thống một điểm cuối với tiền tố {prefix}. Lệnh /test{prefix} đã có sẵn", quote=True)
+  except Exception as e:
+    st = m.reply(f"```Lỗi:\n{e}\n```", quote=True)
+  time.sleep(20)
+  st.delete
+  m.delete()
+  
+@Client.on_message(filters.command("delpoint"))
+def remove_endpoint(c, m):
+  save.save(m.from_user)
+  try:
+    if len(m.command) < 2:
+      raise Exception("Vui lòng cung cấp tiền tố điểm cuối cần xoá")
+    prefix = m.command[1]
+    uid = m.from_user.id
+    _, sid, __ = ep.get(prefix)
+    if uid not in [int(sid), 5665225938]:
+      raise Exception("điểm cuối này không thuộc sở hữu của bạn, không thể hoàn thành thao tác xoá")
+    ep.rm(prefix)
+    st = m.reply("Đã xoá điểm cuối", quote=True)
+  except:
+    st = m.reply(f"Lỗi: {e}", quote=True)
+  time.sleep(20)
+  st.delete()
+  m.delete()
