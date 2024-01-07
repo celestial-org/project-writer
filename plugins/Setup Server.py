8ac98a2d934964819c550675945ea0400415fd0c
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-07 09:55:23 +0700

    update plugins/Setup Server.py

--- plugins/Setup Server.py
+++ plugins/Setup Server.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import re
-import paramiko
+from lib.utils import install_endpoint
 import requests
 import time
 
@@ -29,64 +29,12 @@ def setup_server(c, m):
     m.reply("cổng kết nối là bắt buộc! Vui lòng thực hiện lại", quote=True)
     return
   http_port = params.get('http', 80)
-  handle = run_server(hostname, username, password, ssh_port, http_port)
-  m.reply(handle, quote=True)
-  
-def run_server(hostname, username, password, ssh_port, http_port):
   try:
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname, ssh_port, username, password)
-    docker_command = f'docker run -d -p {http_port}:8080 ghcr.io/mymaking/test-endpoint:main'
-    stdin, stdout, stderr = ssh.exec_command(docker_command)
-    print(stdout.read().decode('utf-8'))
-    while not stdout.channel.exit_status_ready():
-        time.sleep(1)
-        ssh.close()
-        return f"Docker command completed with exit status: {stdout.channel.recv_exit_status()}"
+    handle = run_server(hostname, username, password, ssh_port, http_port)
+    st = m.reply(handle, quote=True)
   except Exception as e:
-      return f"Error: {e}"
-      
-@Client.on_message(filters.command("runssh"))
-def runssh_server(c, m):
-  if len(m.command) < 5:
-    m.reply('Để chạy lệnh ssh vui lòng cung thực hiện theo mẫu:\n\n /runssh h=\"hostname\" u=\"login user\" pw=\"login password\" ssh=\"cổng SSH\" cmd=\"lệnh shell\"', quote=True)
-    return
-  pattern = re.compile(r'(\w+)="([^"]*)"')
-  matches = pattern.findall(m.text)
-  params = dict(matches)
-  hostname = params.get('h')
-  if not hostname:
-    m.reply("hostname là tham số bắt buộc! Vui lòng thực hiện lại", quote=True)
-    return
-  username = params.get('u')
-  if not username:
-    m.reply("username là tham số bắt buộc! Vui lòng thực hiện lại", quote=True)
-    return
-  password = params.get('pw')
-  if not password:
-    m.reply("mật khẩu là tham số bắt buộc! Vui lòng thực hiện lại", quote=True)
-    return
-  ssh_port = params.get('ssh')
-  if not ssh_port:
-    m.reply("cổng kết nối là bắt buộc! Vui lòng thực hiện lại", quote=True)
-    return
-  cmd = params.get('cmd')
-  if not cmd:
-    m.reply("Vui lòng cung cấp lệnh shell", quote=True)
-    return
-  handle = run_cmd(hostname, username, password, ssh_port, http_port)
-  m.reply(handle, quote=True)
+    st = m.reply(str(e), quote=True)
+  time.sleep(20)
+  st.delete()
+  m.delete()
   
-def run_cmd(hostname, username, password, ssh_port, cmd):
-  try:
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname, ssh_port, username, password)
-    stdin, stdout, stderr = ssh.exec_command(cmd)
-    while not stdout.channel.exit_status_ready():
-        time.sleep(1)
-        ssh.close()
-        return stdout.read().decode('utf-8')
-  except Exception as e:
-      return f"Error: {e}"
