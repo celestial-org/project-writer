d001010f7cca8b17509b88089e6facb039d683ad
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-07 13:07:39 +0700

    update lib/lite.py

--- lib/lite.py
+++ lib/lite.py
@@ -2,19 +2,10 @@ import requests
 import base64
 import os
 import io
-
-def local_test(test_url, pingonly=False):
-  if pingonly:
-    cmd = f"./lite -ping 1 -config ./config/ping.json -test {test_url}"
-  else:
-    cmd = f"./lite -ping 1 -config ./config/config.json -test {test_url}"
-  os.system(cmd)
+from lib.env import prox1, prox2
   
 def endpoint_test(test_url, endpoint_url, pingonly=False):
-  if pingonly:
-    data = {"url": test_url, "mode": "ping", "re": 1}
-  else:
-    data = {"url": test_url, "mode": "all", "re": 1}
+  data = {"url": test_url, "mode": "all", "re": 1}
   r = requests.post(endpoint_url, json=data, timeout=10000)
   res = r.json()
   city = res['city']
@@ -31,10 +22,16 @@ def get_config(url):
     res = url
     url = requests.post("https://paste.rs/", data=url).text
   else:
-    res = requests.get(url, headers={"User-Agent": "v2rayNG"})
-    if res.text is None:
-      return None
-    res = res.text
+    try:
+      res = requests.get(url, headers={"User-Agent": "v2rayNG"}, timeout=10)
+      if res.text is None:
+        raise
+      res = res.text
+    except:
+      try:
+        res = requests.get(prox1, params={"url":url}, timeout=10)
+      except:
+        res = requests.get(prox2, params={"url":url})
     if not any(res.startswith(sche) for sche in ["vmess", "trojan", "vless", "ss://"]):
       res = base64.b64decode(res.encode('utf-8')).decode('utf-8')
       url = requests.post("https://paste.rs/", data=res).text
