d291feb5ab40ab63d28ef72a5e89a1359e6f0ad8
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-07 15:49:39 +0700

    update plugins/TestConfig/Endpoints Manage.py

--- plugins/TestConfig/Endpoints Manage.py
+++ plugins/TestConfig/Endpoints Manage.py
@@ -11,10 +11,10 @@ def set_local_endpoint(c, m):
     if len(m.command) > 1:
       ep.get(m.command[1])
       os.environ["ENDPOINT"] = m.command[1]
-      st = m.reply("Đã chuyển đổi điểm cuối mặc định")
+      st = m.reply("Đã chuyển đổi máy chủ test mặc định")
     else:
-      del os.environ["ENDPOINT"]
-      st = m.reply("Đã khôi phục điểm cuối mặc định")
+      os.environ["ENDPOINT"] = "vncmc"
+      st = m.reply("Đã khôi phục máy chủ test")
   except Exception as e:
     st = m.reply(f"Lỗi: {e}", quote=True)
   time.sleep(20)
@@ -37,7 +37,7 @@ def add_endpoint(c, m):
   save.save(m.from_user)
   try:
     if len(m.command) < 3:
-      raise Exception("Vui lòng làm theo mẫu để thêm điểm cuối\n```guide\n/addpoint + tiền tố + url điểm cuối\nví dụ: /addpoint vn http://103.0.0.0:80\n```")
+      raise Exception("Vui lòng làm theo mẫu để thêm máy chủ test\n```guide\n/addpoint + tiền tố + url\nví dụ: /addpoint vn http://103.0.0.0:80\n```")
     prefix = m.command[1]
     if len(prefix) > 5:
       raise Exception("Tiền tố tối đa là 5 ký tự. Vui lòng thừ lại")
@@ -48,7 +48,7 @@ def add_endpoint(c, m):
     sponsor = m.from_user.first_name
     sponsor_id = m.from_user.id
     ep.add(sponsor, sponsor_id, prefix, endpoint)
-    st = m.reply(f"**{sponsor}** đã đóng góp vào hệ thống một điểm cuối với tiền tố {prefix}. Lệnh /test{prefix} đã có sẵn", quote=True)
+    st = m.reply(f"**{sponsor}** đã đóng góp máy chủ test mới với tiền tố {prefix}. Lệnh /test{prefix} đã có thể sử dụng", quote=True)
   except Exception as e:
     st = m.reply(f"```Lỗi:\n{e}\n```", quote=True)
   time.sleep(20)
@@ -60,14 +60,14 @@ def remove_endpoint(c, m):
   save.save(m.from_user)
   try:
     if len(m.command) < 2:
-      raise Exception("Vui lòng cung cấp tiền tố điểm cuối cần xoá")
+      raise Exception("Vui lòng cung cấp prefix máy chủ test cần xoá")
     prefix = m.command[1]
     uid = m.from_user.id
     _, sid, __ = ep.get(prefix)
     if uid not in [int(sid), 5665225938]:
-      raise Exception("điểm cuối này không thuộc sở hữu của bạn, không thể hoàn thành thao tác xoá")
+      raise Exception("Máy chủ test này không thuộc sở hữu của bạn, không thể hoàn thành thao tác xoá")
     ep.rm(prefix)
-    st = m.reply("Đã xoá điểm cuối", quote=True)
+    st = m.reply(f"Đã xoá máy chủ test **{prefix.upper()}**", quote=True)
   except:
     st = m.reply(f"Lỗi: {e}", quote=True)
   time.sleep(20)
