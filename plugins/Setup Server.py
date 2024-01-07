499755a866a9c6980cc02275ff367c287fe0b870
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-07 11:58:01 +0700

    update plugins/Setup Server.py

--- plugins/Setup Server.py
+++ plugins/Setup Server.py
@@ -1,36 +1,21 @@
 from pyrogram import Client, filters
 import re
 from lib.utils import install_endpoint
+from db import savessh
 import requests
 import time
 
 @Client.on_message(filters.command("setupserver"))
 def setup_server(c, m):
-  if len(m.command) < 5:
-    m.reply('Để thiết lập điểm cuối tự động, vui lòng cung thực hiện theo mẫu:\n\n /setupserver h=\"hostname\" u=\"login user\" pw=\"login password\" ssh=\"cổng SSH\" http=\"cổng cài đặt endpoint, mặc định là 80\"', quote=True)
-    return
-  pattern = re.compile(r'(\w+)="([^"]*)"')
-  matches = pattern.findall(m.text)
-  params = dict(matches)
-  hostname = params.get('h')
-  if not hostname:
-    m.reply("hostname là tham số bắt buộc! Vui lòng thực hiện lại", quote=True)
-    return
-  username = params.get('u')
-  if not username:
-    m.reply("username là tham số bắt buộc! Vui lòng thực hiện lại", quote=True)
-    return
-  password = params.get('pw')
-  if not password:
-    m.reply("mật khẩu là tham số bắt buộc! Vui lòng thực hiện lại", quote=True)
-    return
-  ssh_port = params.get('ssh')
-  if not ssh_port:
-    m.reply("cổng kết nối là bắt buộc! Vui lòng thực hiện lại", quote=True)
-    return
-  http_port = params.get('http', 80)
   try:
-    handle = run_server(hostname, username, password, ssh_port, http_port)
+    if len(m.command) < 3:
+     raise Exception("Để thiết lập điểm cuối tự động, vui lòng cung thực hiện theo mẫu:\n\n1) thêm máy chủ SSH bằng lệnh /addserver\n2) chạy lệnh theo mẫu ```cmd\n/setupserver + tên máy + (cổng HTTP endpoint)\n```")
+     
+    user_id = m.from_user.id
+    machine = m.command[1]
+    http_port = m.command[2]
+    hostname, username, password, ssh_port = savessh.get(user_id, machine)
+    handle = install_endpoint(hostname, username, password, ssh_port, http_port)
     st = m.reply(handle, quote=True)
   except Exception as e:
     st = m.reply(str(e), quote=True)
