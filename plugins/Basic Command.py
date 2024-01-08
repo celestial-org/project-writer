6f586fce39c7c3f18c26278c2b82282a99906ef6
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-08 09:08:01 +0700

    update plugins/Basic Command.py

--- plugins/Basic Command.py
+++ plugins/Basic Command.py
@@ -4,16 +4,27 @@ from lib.env import config_tool
 @Client.on_message(filters.command("helps"))
 def help_list(c, m):
   m.reply(f"""Xin chào **{m.from_user.first_name}**(`{m.from_user.id}`), dưới đây là danh sách lệnh khả dụng:
+  
 /get - Lấy liên kết tổng hợp subscribe
+
 /add - Thêm subscribe 
+
 /share - Chia sẻ subscribe 
+
 /mylist - Danh sách subscribe của tôi
+
 /remove - Xoá subscribe khỏi danh sách
+
 /checkall - Tự kiểm tra và xoá subscribe bị lỗi (không bao gồm subscribe hết lưu lượng truy cập)
+
 /request [get, post, put, delete, option] - Gửi yêu cầu HTTP
+
 /sharelist - Lấy danh sách note chung
+
 /rmshare - Xoá subscribe khỏi note chung 
+
 /checkshare - Kiểm tra và xoá khỏi note chung
+
 /ext - Nâng cao
 """, quote=True)
 
@@ -22,13 +33,21 @@ def ext_command_list(c, m):
   m.reply(f"""**{m.from_user.first_name}**(`{m.from_user.id}`), lệnh nâng cao:
   	
 /addpoint + `prefix` + `endpoint url` - Bổ sung máy chủ test
-/endpoints - Lấy danh sách điểm cuối và lệnh gọi
+
+/endpoints - Lấy danh sách lệnh test
+
 /delpoint + `prefix` - Xoá máy chủ test chỉ định 
-/install - Hướng dẫn cài điểm cuối 
+
+/install - Hướng dẫn cài điểm cuối
+
 /setupserver - Cài đặt điểm cuối tự động (cần thêm máy chủ SSH)
+
 /addserver - Thêm máy chủ SHH
+
 /delserver - Xoá máy chủ SSH 
+
 /machines - Danh sách máy chủ SSH đã thêm 
+
 /cmd - Chạy lệnh shell trên máy chủ SSH
 """, quote=True
 )
