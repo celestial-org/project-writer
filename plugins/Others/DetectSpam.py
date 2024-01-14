cfd526e4514f6f85307cd41b30cf9890c8db1c65
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-14 18:25:32 +0700

    update plugins/Others/DetectSpam.py

---
+++ plugins/Others/DetectSpam.py
@@ -0,0 +1,18 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+import requests
+
+def _filter(_, __, m):
+    if m.text > 4000:
+        return True
+    return False
+    
+@Client.on_message(filters.group & filters.create(_filter))
+def detector(c, m):
+    res = requests.post("https://tempnote-1-q9925339.deta.app/post", data=m.text)
+    m.delete()
+    user_id = m.from_user.id
+    name = m.from_user.first_name
+    user = f"[{name}](tg://user?id={user_id})"
+    button = InlineKeyboardMarkup([[InlineKeyboardButton("Bản Sao", web_app=WebAppInfo(url=res.text))]])
+    m.reply(f"Tin nhắn của **{user}** có dấu hiệu spam và đã bị xoá. Dưới đây là bản sao của tin nhắn", reply_markup=button)
