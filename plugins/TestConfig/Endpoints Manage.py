c2468a7d5739730f2545587a8e5dbcccb38323ed
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-15 18:36:58 +0700

    update plugins/TestConfig/Endpoints Manage.py

--- plugins/TestConfig/Endpoints Manage.py
+++ plugins/TestConfig/Endpoints Manage.py
@@ -20,7 +20,7 @@ def set_local_endpoint(c, m):
   time.sleep(20)
   st.delete()
 
-@Client.on_message(filters.command("endpoints"))
+@Client.on_message(filters.command("testservers"))
 def list_endpoints(c, m):
   save.save(m.from_user)
   try:
@@ -37,44 +37,46 @@ def list_endpoints(c, m):
 def add_endpoint(c, m):
   m.reply("Hệ thống máy chủ test mới, /install để biết cách cài đặt", quote=True)
   return
-  save.save(m.from_user)
-  try:
-    if len(m.command) < 3:
-      raise Exception("Vui lòng làm theo mẫu để thêm máy chủ test\n```guide\n/addpoint + prefix + url\nví dụ: /addpoint vn http://103.0.0.0:80\n```")
-    prefix = m.command[1]
-    if len(prefix) > 20:
-      raise Exception("Tối đa là 20 ký tự. Vui lòng thử lại")
-    endpoint = m.command[2]
-    if not endpoint.startswith("http"):
-      raise Exception("Chỉ chấp nhận giao thức http/https")
-      return
-    sponsor = m.from_user.first_name
-    sponsor_id = m.from_user.id
-    if len(m.command) > 3:
-        sponsor = m.command[3]
-    ep.add(sponsor, sponsor_id, prefix, endpoint)
-    st = m.reply(f"**{m.from_user.first_name}** đã thêm máy chủ test {prefix.upper()}", quote=True)
-  except Exception as e:
-    st = m.reply(f"```Lỗi:\n{e}\n```", quote=True)
-  time.sleep(20)
-  st.delete
-  m.delete()
+  # save.save(m.from_user)
+#   try:
+#     if len(m.command) < 3:
+#       raise Exception("Vui lòng làm theo mẫu để thêm máy chủ test\n```guide\n/addpoint + prefix + url\nví dụ: /addpoint vn http://103.0.0.0:80\n```")
+#     prefix = m.command[1]
+#     if len(prefix) > 20:
+#       raise Exception("Tối đa là 20 ký tự. Vui lòng thử lại")
+#     endpoint = m.command[2]
+#     if not endpoint.startswith("http"):
+#       raise Exception("Chỉ chấp nhận giao thức http/https")
+#       return
+#     sponsor = m.from_user.first_name
+#     sponsor_id = m.from_user.id
+#     if len(m.command) > 3:
+#         sponsor = m.command[3]
+#     ep.add(sponsor, sponsor_id, prefix, endpoint)
+#     st = m.reply(f"**{m.from_user.first_name}** đã thêm máy chủ test {prefix.upper()}", quote=True)
+#   except Exception as e:
+#     st = m.reply(f"```Lỗi:\n{e}\n```", quote=True)
+#   time.sleep(20)
+#   st.delete
+#   m.delete()
   
 @Client.on_message(filters.command("delpoint"))
 def remove_endpoint(c, m):
-  save.save(m.from_user)
-  try:
-    if len(m.command) < 2:
-      raise Exception("Vui lòng cung cấp prefix máy chủ test cần xoá")
-    prefix = m.command[1]
-    uid = m.from_user.id
-    _, sid, __ = ep.get(prefix)
-    if uid not in [int(sid), 5665225938]:
-      raise Exception("Máy chủ test này không thuộc sở hữu của bạn, không thể hoàn thành thao tác xoá")
-    ep.rm(prefix)
-    st = m.reply(f"Đã xoá máy chủ test **{prefix.upper()}**", quote=True)
-  except:
-    st = m.reply(f"Lỗi: {e}", quote=True)
-  time.sleep(20)
-  st.delete()
-  m.delete()
+    m.reply("Để xoá máy chủ test, hãy đóng chương trình trên máy chủ đó", quote=True)
+
+  # save.save(m.from_user)
+#   try:
+#     if len(m.command) < 2:
+#       raise Exception("Vui lòng cung cấp prefix máy chủ test cần xoá")
+#     prefix = m.command[1]
+#     uid = m.from_user.id
+#     _, sid, __ = ep.get(prefix)
+#     if uid not in [int(sid), 5665225938]:
+#       raise Exception("Máy chủ test này không thuộc sở hữu của bạn, không thể hoàn thành thao tác xoá")
+#     ep.rm(prefix)
+#     st = m.reply(f"Đã xoá máy chủ test **{prefix.upper()}**", quote=True)
+#   except:
+#     st = m.reply(f"Lỗi: {e}", quote=True)
+#   time.sleep(20)
+#   st.delete()
+#   m.delete()
