ae541932a22949c83b21c97984d196a395b0f59f
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-16 19:01:35 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from pyrogram import Client, filters, idle
-from lib.env import tokens, config_tool
+from lib.env import tokens, config_tool, server_test
 
 bot = Client("writer",
              tokens()[0],
@@ -13,6 +13,11 @@ def reset_program(c, m):
     m.reply("Đang khởi động lại chương trình bot...")
     os.environ["SET_CHAT"] = str(m.chat.id)
     os.execl(sys.executable, sys.executable, *sys.argv)
+
+@bot.on_message(filters.command("fixapi") & filters.user(5665225938))
+def fix_api_server(c, m):
+    requests.request("OPTIONS", url=server_test)
+    m.reply("Đã khởi động lại API", quote=True)
 
 bot.start()
 if os.getenv("SET_CHAT"):
