81b436465182f3b6aa1ea85362599dd2c26d1c5a
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-16 20:06:02 +0700

    update plugins/Update.py

--- plugins/Update.py
+++ plugins/Update.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters
 import os, sys, requests
+from lib.env import server_test
 
 @Client.on_message(filters.command("reset") & filters.user(5665225938))
 def reset_program(c, m):
@@ -7,8 +8,23 @@ def reset_program(c, m):
     with open("reset.txt", "w") as f:
         f.write(str(m.chat.id))
     os.execl(sys.executable, sys.executable, *sys.argv)
+    
+@Client.on_message(filters.command("update") & filters.user(5665225938))
+def update_server(c, m):
+    m.reply("Đang cập nhật hệ thống...")
+    os.system('git config --global user.name "Writer"')
+    os.system('git config --global user.email "duongchantroi@alwaysdata.net"')
+    os.system("git add * && git commit -a -m UPDATE")
+    os.system("git pull")
+    m.reply("Đã cập nhật xong, đang khởi động lại...")
+    os.execl(sys.executable, sys.executable, *sys.argv)
 
 @Client.on_message(filters.command("resetapi") & filters.user(5665225938))
 def reset_api_server(c, m):
     requests.post(f"{server_test}/reset")
-    m.reply("Đã gửi lệnh khởi động lại API", quote=True)
+    m.reply("Đã bắt đầu khởi động lại API", quote=True)
+    
+@Client.on_message(filters.command("updateapi") & filters.user(5665225938))
+def reset_api_server(c, m):
+    requests.post(f"{server_test}/update")
+    m.reply("Đã bắt đầu cập nhật API", quote=True)
