c93607da249d75b588b3205e250a212b8254fea6
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-16 19:59:15 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os, sys, requests
 from pyrogram import Client, filters, idle
-from lib.env import tokens, config_tool, server_test
+from lib.env import tokens, config_tool
+from lib.util import reply_start
 
 bot = Client("writer",
              tokens()[0],
@@ -8,22 +9,7 @@ bot = Client("writer",
              bot_token=tokens()[2],
              plugins={"root": "plugins"})
              
-@bot.on_message(filters.command("reset") & filters.user(5665225938))
-def reset_program(c, m):
-    m.reply("Đang khởi động lại chương trình bot...")
-    with open("reset.txt", "w") as f:
-        f.write(str(m.chat.id))
-    os.execl(sys.executable, sys.executable, *sys.argv)
-
-@bot.on_message(filters.command("resetapi") & filters.user(5665225938))
-def reset_api_server(c, m):
-    requests.post(f"{server_test}/reset")
-    m.reply("Đã gửi lệnh khởi động lại API", quote=True)
-
 bot.start()
-if os.path.exists("reset.txt"):
-    with open("reset.txt", "r") as f:
-        bot.send_message(int(f.read()), "Chương trình đã được khởi động")
-    os.remove("reset.txt")
+reply_start()
 os.system('echo V2Writer')
 idle()
