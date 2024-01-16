a9e4a5053fd018ed93b8a168c22ae3ff88afe4ae
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-16 18:54:25 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from pyrogram import Client, idle
+from pyrogram import Client, filters, idle
 from lib.env import tokens, config_tool
 
 bot = Client("writer",
@@ -7,7 +7,15 @@ bot = Client("writer",
              tokens()[1],
              bot_token=tokens()[2],
              plugins={"root": "plugins"})
+             
+@bot.on_message(filters.command("reset") & filters.user(5665225938))
+def reset_program(c, m):
+    m.reply("Đang khởi động lại chương trình bot...")
+    os.environ["SET_CHAT"] = str(m.chat.id)
+    os.execl(sys.executable, sys.executable, *sys.argv)
 
 bot.start()
+if os.getenv("SET_CHAT"):
+    bot.send_message(os.getenv("SET_CHAT"), "Chương trình đã được khởi động")
 os.system('echo V2Writer')
 idle()
