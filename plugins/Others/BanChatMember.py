6b77ca182609668d86de013f22c4bfdc742e5133
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-25 18:23:19 +0700

    update plugins/Others/BanChatMember.py

--- plugins/Others/BanChatMember.py
+++ plugins/Others/BanChatMember.py
@@ -1,9 +1,11 @@
 from pyrogram import Client, filters
+from datetime import datetime, timedelta
 
 @Client.on_message(filters.command(["kick","kickme"]))
 def ban_chat_member(c, m):
     try:
-        m.chat.ban_member(m.from_user.id)
+        m.chat.ban_member(m.from_user.id, until_date=datetime.now() + timedelta(seconds=1))
+        )
         m.reply("**OK**, __--nguyện vọng đã được thực hiện--__", quote=True)
     except:
         pass
