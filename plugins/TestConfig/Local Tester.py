cf0713bdab90f8aefdfe04ac042ce08c31a92c23
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-26 11:17:54 +0700

    update plugins/TestConfig/Local Tester.py

--- plugins/TestConfig/Local Tester.py
+++ plugins/TestConfig/Local Tester.py
@@ -1,5 +1,6 @@
 from hydrogram import Client, filters
 from lib.lite import get_config, start_test, get_endpoints, check_before
+from hydrogram.types import ChatAction
 import subprocess
 import time
 import requests
@@ -14,64 +15,66 @@ def ranpoint():
 
 @Client.on_message(filters.command("test"))
 def run_lite_command(c, m):
-  prefix = os.getenv("ENDPOINT")
-  url_pattern = re.compile(r'((http[s]?|vmess|trojan|vless|ss)://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)')
-  if m.reply_to_message:
+    m.reply_chat_action(ChatAction.TYPING)    
+    prefix = os.getenv("ENDPOINT")
+    url_pattern = re.compile(r'((http[s]?|vmess|trojan|vless|ss)://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)')
+    if m.reply_to_message:
     try:
-      text = m.reply_to_message.text
+        text = m.reply_to_message.text
     except:
-      try:
+        try:
         text = m.reply_to_message.caption
-      except:
+        except:
         m.reply("Không tìm thấy tin nhắn văn bản", quote=True)
         return
-  else:
+    else:
     try:
      text = m.text
     except:
-      try:
+        try:
         text = m.caption
-      except:
+        except:
         m.reply("Không tìm thấy tin nhắn văn bản", quote=True)
         return
-  matches = re.findall(url_pattern, text)
-  urls = [match[0] for match in matches]
-  if not urls:
+    matches = re.findall(url_pattern, text)
+    urls = [match[0] for match in matches]
+    if not urls:
     m.reply("Không tìm thấy URL trong tin nhắn văn bản", quote=True)
     return
-  for url in urls:
-    try:
-      test_url, count = get_config(url)
-    except:
-      uvl = m.reply("Liên kết không khả dụng", quote=True)
-      time.sleep(10)
-      uvl.delete()
-      return
-    if count is None:
-      m.reply("Liên kết bị lỗi", quote=True)
-      return
-    if not prefix:
+    for url in urls:
+        m.reply_chat_action(ChatAction.TYPING)
         try:
-            prefix = ranpoint()
+            test_url, count = get_config(url)
         except:
-            m.reply("Không có máy chủ test nào hoạt động cả", quote=True)
+            uvl = m.reply("Liên kết không khả dụng", quote=True)
+            time.sleep(10)
+            uvl.delete()
             return
-    try:
-      endpoint = check_before(prefix)
-    except Exception as e:
-      stt = m.reply(f"{e}. Hãy thử máy chủ test khác", quote=True)
-      time.sleep(10)
-      stt.delete()
-      return
-    if url.startswith("http"):
-      stt = m.reply(f'**{m.from_user.first_name}** thực hiện test liên kết {url} với **{count}** cấu hình\nPrefix: **{prefix.upper()}**', quote=True)
-    else:
-      stt = m.reply(f'**{m.from_user.first_name}** thực hiện test 1 cấu hình\n{test_url}\nPrefix: **{prefix.upper()}**', quote=True)
-    try:
-        location, org, sponsor, photo = start_test(test_url, endpoint)
-    except Exception as e:
-        m.reply(f"**Lỗi máy chủ:** ```{e}```")
-        return
-    m.reply_photo(photo=photo, quote=True, caption=f"```sponsor\n{sponsor}\n```\nVị trí: **{location}**\nTổ chức: **{org}**\nTest bởi **[{m.from_user.first_name}](tg://user?id={m.from_user.id})**")
-    time.sleep(5)
-    stt.delete()
+        if count is None:
+            m.reply("Liên kết bị lỗi", quote=True)
+            return
+        if not prefix:
+            try:
+                prefix = ranpoint()
+            except:
+                m.reply("Không có máy chủ test nào hoạt động cả", quote=True)
+                return
+        try:
+            endpoint = check_before(prefix)
+        except Exception as e:
+            stt = m.reply(f"{e}. Hãy thử máy chủ test khác", quote=True)
+            time.sleep(10)
+            stt.delete()
+            return
+        if url.startswith("http"):
+            stt = m.reply(f'**{m.from_user.first_name}** thực hiện test liên kết {url} với **{count}** cấu hình\nPrefix: **{prefix.upper()}**', quote=True)
+        else:
+            stt = m.reply(f'**{m.from_user.first_name}** thực hiện test 1 cấu hình\n{test_url}\nPrefix: **{prefix.upper()}**', quote=True)
+        try:
+            location, org, sponsor, photo = start_test(test_url, endpoint)
+        except Exception as e:
+            m.reply(f"**Lỗi máy chủ:** ```{e}```")
+            return
+        m.reply_photo(photo=photo, quote=True, caption=f"```sponsor\n{sponsor}\n```\nVị trí: **{location}**\nTổ chức: **{org}**\nTest bởi **[{m.from_user.first_name}](tg://user?id={m.from_user.id})**")
+        time.sleep(5)
+        stt.delete()
