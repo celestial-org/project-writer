8eabbd2a686fd440986735b821b43389288da52b
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-26 11:26:09 +0700

    update plugins/Para Miko.py

--- plugins/Para Miko.py
+++ plugins/Para Miko.py
@@ -1,4 +1,5 @@
 from hydrogram import Client, filters, enums
+from hydrogram.enums import ChatAction
 import re
 from lib.utils import run_cmd
 import requests
@@ -7,77 +8,80 @@ from db import savessh
 
 @Client.on_message(filters.command("addserver"))
 def save_ssh_login(c, m):
-  try:
-    if m.chat.type != enums.ChatType.PRIVATE:
-      raise Exception("Để bảo mật, vui lòng thực hiện thao tác này ở chat riêng tư")
-    if len(m.command) < 5:
-      raise Exception("Không đủ tham số\nVui lòng thực hiện theo mẫu:\n/addserver + `machine name` + `hostname/ip` + `ssh user` + `ssh password` + `ssh port(nếu là 22 thì không cần nhập)`")
-    if len(m.command) == 5:
-      _, machine, host, sshuser, passwd = m.command
-      port = 22
-    else:
-      _, machine, host, sshuser, passwd, port = m.command
-    user_id = m.from_user.id
-    savessh.add(user_id, machine, host, sshuser, passwd, port)
-    st = m.reply(f"Máy chủ với tên {machine} đã được lưu", quote=True)
-    time.sleep(10)
-  except Exception as e:
-    st = m.reply(str(e), quote=True)
-  time.sleep(60)
-  st.delete()
-  m.delete()
-  
-@Client.on_message(filters.command("delserver"))
-def delete_machine_server(c, m):
-  try:
-    if len(m.command) < 2:
-      raise Exception("Vui lòng cung cấp tên máy")
-    machine = m.command[1]
-    user_id = m.from_user.id
+    m.reply_chat_action(ChatAction.TYPING)
     try:
-      savessh.delete(user_id, machine)
+        if m.chat.type != enums.ChatType.PRIVATE:
+            raise Exception("Để bảo mật, vui lòng thực hiện thao tác này ở chat riêng tư")
+        if len(m.command) < 5:
+            raise Exception("Không đủ tham số\nVui lòng thực hiện theo mẫu:\n/addserver + `machine name` + `hostname/ip` + `ssh user` + `ssh password` + `ssh port(nếu là 22 thì không cần nhập)`")
+        if len(m.command) == 5:
+            _, machine, host, sshuser, passwd = m.command
+            port = 22
+        else:
+            _, machine, host, sshuser, passwd, port = m.command
+        user_id = m.from_user.id
+        savessh.add(user_id, machine, host, sshuser, passwd, port)
+        st = m.reply(f"Máy chủ với tên {machine} đã được lưu", quote=True)
+        time.sleep(10)
     except Exception as e:
-      raise Exception(str(e))
-    st = m.reply(f"Đã xoá máy chủ {machine}", quote=True)
-  except Exception as e:
-    st = m.reply(str(e), quote=True)
-  time.sleep(20)
-  st.delete()
-  m.delete()
-
-@Client.on_message(filters.command("machines"))
-def get_list_machines(c, m):
-  try:
-    user_id = m.from_user.id
-    user = m.from_user.first_name
-    list_machines = savessh.machines(user_id)
-    list_machines = "  ".join(list_machines)
-    m.reply(f"Danh sách máy chủ SSH của **{user}**:```Machines\n{list_machines}\n```", quote=True)
-  except Exception as e:
-    st = m.reply(str(e), quote=True)
-    time.sleep(10)
-    st.delete()
-    
-@Client.on_message(filters.command("cmd"))
-def run_shell_command(c, m):
-  try:
-    if len(m.command) < 3:
-      raise Exception("Thiếu tham số\n\n/cmd + `tên máy` + `lệnh shell`")
-    user_id = m.from_user.id
-    machine = m.command[1]
-    shell_cmd = m.command[2]
-    host, sshuser, passwd, port = savessh.get(user_id, machine)
-    result = run_cmd(host, sshuser, passwd, port, shell_cmd)
-    max_length = 4000
-    if len(result) > max_length:
-      parts = [text[i:i+max_length] for i in range(0, len(text), max_length)]
-      for i, part in enumerate(parts, start=1):
-        m.reply(f"{i}```bash\n{part}\n```", quote=True)
-    else:
-      m.reply(f"```bash\n{result}\n```", quote=True)
-  except Exception as e:
-    st = m.reply(f"```bash\n{e}\n```", quote=True)
-    time.sleep(20)
+        st = m.reply(str(e), quote=True)
+    time.sleep(60)
     st.delete()
     m.delete()
     
+@Client.on_message(filters.command("delserver"))
+def delete_machine_server(c, m):
+    m.reply_chat_action(ChatAction.TYPING)
+    try:
+        if len(m.command) < 2:
+            raise Exception("Vui lòng cung cấp tên máy")
+        machine = m.command[1]
+        user_id = m.from_user.id
+        try:
+            savessh.delete(user_id, machine)
+        except Exception as e:
+            raise Exception(str(e))
+        st = m.reply(f"Đã xoá máy chủ {machine}", quote=True)
+    except Exception as e:
+        st = m.reply(str(e), quote=True)
+    time.sleep(20)
+    st.delete()
+    m.delete()
+
+@Client.on_message(filters.command("machines"))
+def get_list_machines(c, m):
+    try:
+        user_id = m.from_user.id
+        user = m.from_user.first_name
+        list_machines = savessh.machines(user_id)
+        list_machines = "    ".join(list_machines)
+        m.reply(f"Danh sách máy chủ SSH của **{user}**:```Machines\n{list_machines}\n```", quote=True)
+    except Exception as e:
+        st = m.reply(str(e), quote=True)
+        time.sleep(10)
+        st.delete()
+        
+@Client.on_message(filters.command("cmd"))
+def run_shell_command(c, m):
+    m.reply_chat_action(ChatAction.TYPING)
+    try:
+        if len(m.command) < 3:
+            raise Exception("Thiếu tham số\n\n/cmd + `tên máy` + `lệnh shell`")
+        user_id = m.from_user.id
+        machine = m.command[1]
+        shell_cmd = m.command[2]
+        host, sshuser, passwd, port = savessh.get(user_id, machine)
+        result = run_cmd(host, sshuser, passwd, port, shell_cmd)
+        max_length = 4000
+        if len(result) > max_length:
+            parts = [text[i:i+max_length] for i in range(0, len(text), max_length)]
+            for i, part in enumerate(parts, start=1):
+                m.reply(f"{i}```bash\n{part}\n```", quote=True)
+        else:
+            m.reply(f"```bash\n{result}\n```", quote=True)
+    except Exception as e:
+        st = m.reply(f"```bash\n{e}\n```", quote=True)
+        time.sleep(20)
+        st.delete()
+        m.delete()
+        
