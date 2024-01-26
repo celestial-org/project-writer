ce9f5585fccb6d88d8839f8ab8864b3651b50c4c
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-26 11:28:11 +0700

    update plugins/Others/BanChatMember.py

--- plugins/Others/BanChatMember.py
+++ plugins/Others/BanChatMember.py
@@ -1,8 +1,10 @@
 from hydrogram import Client, filters
+from hydrogram.enums import ChatAction
 from datetime import datetime, timedelta
 
 @Client.on_message(filters.command(["kick","kickme"]))
 def ban_chat_member(c, m):
+    m.reply_chat_action(ChatAction.TYPING)
     try:
         m.chat.ban_member(m.from_user.id, until_date=datetime.now() + timedelta(seconds=30))
         m.reply("**OK**, __--nguyện vọng đã được thực hiện--__", quote=True)
