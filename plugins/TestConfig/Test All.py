237012bb84f170fed608778d7880452c726ba97e
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-26 11:33:40 +0700

    update plugins/TestConfig/Test All.py

--- plugins/TestConfig/Test All.py
+++ plugins/TestConfig/Test All.py
@@ -22,55 +22,55 @@ def run_lite_command_test_all(c, m):
     m.reply_chat_action(ChatAction.TYPING)
     url_pattern = re.compile(r'((http[s]?|vmess|trojan|vless|ss)://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)')
     if m.reply_to_message:
-    try:
-        text = m.reply_to_message.text
-    except:
         try:
-        text = m.reply_to_message.caption
+            text = m.reply_to_message.text
         except:
-        m.reply("Không tìm thấy tin nhắn văn bản", quote=True)
-        return
+            try:
+            text = m.reply_to_message.caption
+            except:
+            m.reply("Không tìm thấy tin nhắn văn bản", quote=True)
+            return
     else:
-    try:
-     text = m.text
-    except:
         try:
-        text = m.caption
+         text = m.text
         except:
-        m.reply("Không tìm thấy tin nhắn văn bản", quote=True)
-        return
+            try:
+            text = m.caption
+            except:
+            m.reply("Không tìm thấy tin nhắn văn bản", quote=True)
+            return
     matches = re.findall(url_pattern, text)
     urls = [match[0] for match in matches]
     if not urls:
-    m.reply("Không tìm thấy URL trong tin nhắn văn bản", quote=True)
-    return
+        m.reply("Không tìm thấy URL trong tin nhắn văn bản", quote=True)
+        return
     for url in urls:
-    try:
-        test_url, count = get_config(url)
-    except:
-        uvl = m.reply("Liên kết không khả dụng", quote=True)
-        time.sleep(10)
-        uvl.delete()
-        return
-    if count is None:
-        m.reply("Liên kết bị lỗi", quote=True)
-        return
-    if url.startswith("http"):
-        stt = m.reply(f'**{m.from_user.first_name}** vừa bắt đầu đợt kiểm tra mới đến liên kết {url} với **{count}** cấu hình.\n**ALL**', quote=True)
-    else:
-        stt = m.reply(f'**{m.from_user.first_name}** vừa bắt đầu đợt kiểm tra mới đến 1 cấu hình\n{test_url}.\nMáy chủ test: **ALL**', quote=True)
-    _, endpoints, __ = get_endpoints()
-    msg_list = []
-    for prefix in endpoints:
         try:
-            endpoint = check_before(prefix)
-            location, org, sponsor, photo = start_test(test_url, endpoint)
+            test_url, count = get_config(url)
         except:
-            continue
-        msg = (photo, f"```sponsor\n{sponsor}\n```\nVị trí: **{location}**\nTổ chức: **{org}**\nTest bởi **[{m.from_user.first_name}](tg://user?id={m.from_user.id})**")
-        msg_list.append(msg)
-    for msg in msg_list:
-        m.reply_chat_action(ChatAction.TYPING)
-        m.reply_photo(photo=msg[0], caption=msg[1], quote=True)
-    time.sleep(5)
-    stt.delete()
+            uvl = m.reply("Liên kết không khả dụng", quote=True)
+            time.sleep(10)
+            uvl.delete()
+            return
+        if count is None:
+            m.reply("Liên kết bị lỗi", quote=True)
+            return
+        if url.startswith("http"):
+            stt = m.reply(f'**{m.from_user.first_name}** vừa bắt đầu đợt kiểm tra mới đến liên kết {url} với **{count}** cấu hình.\n**ALL**', quote=True)
+        else:
+            stt = m.reply(f'**{m.from_user.first_name}** vừa bắt đầu đợt kiểm tra mới đến 1 cấu hình\n{test_url}.\nMáy chủ test: **ALL**', quote=True)
+        _, endpoints, __ = get_endpoints()
+        msg_list = []
+        for prefix in endpoints:
+            try:
+                endpoint = check_before(prefix)
+                location, org, sponsor, photo = start_test(test_url, endpoint)
+            except:
+                continue
+            msg = (photo, f"```sponsor\n{sponsor}\n```\nVị trí: **{location}**\nTổ chức: **{org}**\nTest bởi **[{m.from_user.first_name}](tg://user?id={m.from_user.id})**")
+            msg_list.append(msg)
+        for msg in msg_list:
+            m.reply_chat_action(ChatAction.TYPING)
+            m.reply_photo(photo=msg[0], caption=msg[1], quote=True)
+        time.sleep(5)
+        stt.delete()
