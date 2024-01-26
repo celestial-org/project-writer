4d11b1fce0b8df96adbb9282b313e6a02a9cf139
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-26 11:13:54 +0700

    Delete the file at 'db/save.py'

--- db/save.py
+++
@@ -1,9 +0,0 @@
-from db.base import note_users as db
-
-
-def save(user):
-  name = user.first_name
-  user_id = user.id
-  username = user.username
-  data = {"username": username, "name": name}
-  db.put(data=data, key=str(user_id))
