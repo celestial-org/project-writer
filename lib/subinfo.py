5fd1aaba1d8a025ccac58bb2924db0d7aa5f7ca9
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-26 17:16:37 +0700

    update lib/subinfo.py

--- lib/subinfo.py
+++ lib/subinfo.py
@@ -26,6 +26,7 @@ def parse_url(url):
     except:
         res_text = r.text
     res_text = res_text.splitlines()
+    res_text = list(filter(lambda x: "://" in x, res_text))
     result_dict = {}
     orgi_dict = {}
     if res_string:
