ef6a0d8fd95733f90e17da41418de882eaf4f54c
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-27 21:32:16 +0700

    update db/base.py

--- db/base.py
+++ db/base.py
@@ -2,7 +2,5 @@ from deta import Deta
 from lib.env import deta_key
 
 deta = Deta(deta_key)
-v2ray_notes = deta.Base("v2ray-notes")
-note_users = deta.Base("note-users")
-endpoints = deta.Base("endpoints")
+v2ray_notes = deta.Base("notes")
 savessh = deta.Base("ssh")
