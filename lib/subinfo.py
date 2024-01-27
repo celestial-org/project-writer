d366edff6705a40e443c3b763e595ccdb7e2273c
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-27 10:48:28 +0700

    update lib/subinfo.py

--- lib/subinfo.py
+++ lib/subinfo.py
@@ -21,7 +21,7 @@ def convert_timestamp_to_datetime(timestamp, timezone='UTC'):
 def parse_url(url):
     r = requests.get(url, headers={"User-Agent": "clash"}, proxies={"http": "http://127.0.0.1:8888", "https": "http://127.0.0.1:8888"}, timeout=60)
     res_string = r.headers.get("subscription-userinfo")
-    r = requests.get(url, headers={"User-Agent": "v2rayN"}, proxies={"http": "http://127.0.0.1:8888", "https": "http://127.0.0.1:8888"}, timeout=60)
+    r = requests.get(url, headers={"User-Agent": "v2rayNG"}, proxies={"http": "http://127.0.0.1:8888", "https": "http://127.0.0.1:8888"}, timeout=60)
     try:
         res_text = base64.b64decode(r.text.encode('ascii', 'ignore'))
     except:
