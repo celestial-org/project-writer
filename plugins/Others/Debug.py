947bf7eb853929152b4c5307f8f56f36103b33fc
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-31 09:32:51 +0700

    update plugins/Others/Debug.py

--- plugins/Others/Debug.py
+++ plugins/Others/Debug.py
@@ -1,5 +1,6 @@
 from hydrogram import Client, filters
+from hydrogram.enums import ParseMode
 
 @Client.on_message(filters.command("debug"))
 def debugger(c,m):
-  m.reply(f">>{m}<<")
+  m.reply(f"<blockquote>{m}</blockquote>", parse_mode=ParseMode.HTML)
