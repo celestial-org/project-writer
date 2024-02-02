1e359caf7d22409cf5187c0edbb0723f3a7f9735
chantroi <tonglanhchua@gmail.com>
2024-02-02 20:16:55 +0700

    update plugins/TrackURL.py

--- plugins/TrackURL.py
+++ plugins/TrackURL.py
@@ -3,13 +3,12 @@ from hydrogram.enums import MessageEntityType
 import requests
 import shelve
 
-db = shelve.open("message.db")
-
 def is_url(_, __, m):
     return any(entity.type == MessageEntityType.URL for entity in m.entities)
 
 @Client.on_message(filters.create(is_url), group=3)
 def get_all(c, m):
+    db = shelve.open("message.db")
     for url in [url for url in m.text.split(None) if any(scheme in url for scheme in ["http://", "https://"])]:
         r = requests.get(url, headers={"User-Agent": "Writer/Telegram Bot"}, proxies={"http": "http://127.0.0.1:8888", "https": "http://127.0.0.1:8888"})
         headers = "\n".join([f"{k.upper()}: {v}" for k, v in r.headers.items()])
@@ -19,6 +18,7 @@ def get_all(c, m):
         
 @Client.on_deleted_messages(group=3)
 def delete_self_msg(c, m):
+    db = shelve.open("message.db")
     if not isinstance(m, list):
         if any(str(m.id) in list(db.keys())):
             ct_id = db[str(m.id)]
