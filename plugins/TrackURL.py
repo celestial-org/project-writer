c9254df8060cbbae250c4a06379eeb9d65d731f3
chantroi <tonglanhchua@gmail.com>
2024-02-02 20:44:23 +0700

    update plugins/TrackURL.py

--- plugins/TrackURL.py
+++ plugins/TrackURL.py
@@ -9,15 +9,14 @@ def is_url(_, __, m):
 
 @Client.on_message(filters.create(is_url), group=3)
 def get_all(c, m):
-    with shelve.open("message.db") as db:
-        for url in [url for url in m.text.split(None) if any(scheme in url for scheme in ["http://", "https://"])]:
-            r = requests.get(url, headers={"User-Agent": "Writer/Telegram Bot"}, proxies={"http": "http://127.0.0.1:8888", "https": "http://127.0.0.1:8888"})
-            headers = "\n".join([f"{k.upper()}: {v}" for k, v in r.headers.items()])
-            text = f"```\n{headers}```"
-            ct = m.reply(text, quote=True)
-            #db[str(m.id)]=str(ct.id)
-            time.sleep(30)
-            ct.delete()
+    for url in [url for url in m.text.split(None) if any(scheme in url for scheme in ["http://", "https://"])]:
+        r = requests.get(url, headers={"User-Agent": "Writer/Telegram Bot"}, proxies={"http": "http://127.0.0.1:8888", "https": "http://127.0.0.1:8888"})
+        headers = "\n".join([f"{k.upper()}: {v}" for k, v in r.headers.items()])
+        text = f"```\n{headers}```"
+        ct = m.reply(text, quote=True)
+        #db[str(m.id)]=str(ct.id)
+        time.sleep(30)
+        ct.delete()
         
 @Client.on_deleted_messages(group=3)
 def delete_self_msg(c, m):
