2fffa2592a82c5920a7fceec1d87328a41036425
chantroi <tonglanhchua@gmail.com>
2024-02-11 08:16:37 +0700

    update lib/env.py

--- lib/env.py
+++ lib/env.py
@@ -1,16 +1,21 @@
 import os
 from deta import Deta
+import requests
+
+secret = os.getenv("SECRET")
+res = requests.get(secret).json()
 
 config_tool = os.getenv("CONFIG_TOOL")
-deta_key = os.getenv("DETA_KEY")
+deta_key = res["key"]["collection"]
+
 prox1 = os.getenv("PROX1")
 prox2 = os.getenv("PROX2")
 server_test = os.getenv("SERVER_TEST")
 
 deta = Deta(deta_key)
-base = deta.Base('tokens')
+
 def tokens():
-  api_id = base.get('api')['id']
-  api_hash = base.get('api')['hash']
-  bot_token = base.get('bot')["nw"]
+  api_id = res["key"]["api_id"]
+  api_hash = res["key"]["api_hash"]
+  bot_token = res["bot"]["nw_tg"]
   return api_id, api_hash, bot_token
