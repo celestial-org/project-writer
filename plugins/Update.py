37b1b51a0365b51b6bc612e2c4fb7dca49b0c8b5
chantroi <tonglanhchua@gmail.com>
2024-02-14 18:15:50 +0700

    update plugins/Update.py

--- plugins/Update.py
+++ plugins/Update.py
@@ -14,6 +14,7 @@ def update_server(c, m):
     m.reply("Đang cập nhật hệ thống...")
     os.system('git config --global user.name "Writer"')
     os.system('git config --global user.email "duongchantroi@alwaysdata.net"')
+    os.system('git config --global pull.rebase true')
     os.system("git add * && git commit -a -m UPDATE")
     os.system("git pull")
     m.reply("Đã cập nhật xong, đang khởi động lại...")
