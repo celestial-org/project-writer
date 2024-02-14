497ba2be73f96707a846c95ae83a395f1166899a
chantroi <tonglanhchua@gmail.com>
2024-02-14 18:11:50 +0700

    update plugins/Others/ServerInfo.py

--- plugins/Others/ServerInfo.py
+++ plugins/Others/ServerInfo.py
@@ -17,7 +17,7 @@ def server_info():
     parts = ip.split('.')
     ip = ".".join(parts[:2] + ['*.*'])
 
-    server_info = f"IP: \nCity: {city}\nRegion: {region}\nCountry: {country}\nProvider: {provider}"
+    server_info = f"IP: {ip}\nCity: {city}\nRegion: {region}\nCountry: {country}\nProvider: {provider}"
 
   except Exception as e:
     server_info = f"Error: {e}"
