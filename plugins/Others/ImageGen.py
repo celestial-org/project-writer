81b9ca2444cb166262a97dcf4e392aacea85a9bf
chantroi <tonglanhchua@gmail.com>
2024-02-19 15:34:20 +0700

    update plugins/Others/ImageGen.py

--- plugins/Others/ImageGen.py
+++ plugins/Others/ImageGen.py
@@ -1,12 +1,15 @@
 from hydrogram import Client, filters
+from hydrogram.enums import ChatAction
 from io import BytesIO
 import requests
 
 @Client.on_message(filters.command("image"))
 def image_generator(c, m):
     if len(m.command) > 1:
+        m.reply_chat_action(ChatAction.TYPING)
         query = m.text.split(m.command[0])[1]
         response = requests.get("https://diffusion.cloudlapse.workers.dev/", params=dict(q=query))
+        m.reply_chat_action(ChatAction.UPLOAD_PHOTO)
         if m.from_user:
             name = m.from_user.first_name
         else:
