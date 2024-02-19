01f0cdaf4378a5028cdea0e75383b1b990bf8982
chantroi <tonglanhchua@gmail.com>
2024-02-19 15:13:00 +0700

    update plugins/Others/ImageGen.py

--- plugins/Others/ImageGen.py
+++ plugins/Others/ImageGen.py
@@ -4,14 +4,15 @@ import requests
 
 @Client.on_message(filters.command("image"))
 def image_generator(c, m):
-    q = m.from_user.ask("Hãy nhập lời nhắc để tạo ảnh", filters=filters.text)
-    response = requests.get("https://diffusion.cloudlapse.workers.dev/", params=dict(q=q.text))
-    if m.from_user:
-        name = m.from_user.first_name
-    else:
-        name = "Channel/Group"
-    if response.status_code == 200:
-        result = BytesIO(response.content)
-        result.name = "image.png"
-        q.reply_photo(result, caption=f"By __**{name}**__", quote=True)
-    m.delete()
+    if len(m.command) > 1:
+        q = m.text.split(m.command[0])[1]
+        response = requests.get("https://diffusion.cloudlapse.workers.dev/", params=dict(q=q.text))
+        if m.from_user:
+            name = m.from_user.first_name
+        else:
+            name = "Channel/Group"
+        if response.status_code == 200:
+            result = BytesIO(response.content)
+            result.name = "image.png"
+            q.reply_photo(result, caption=f"By __**{name}**__", quote=True)
+        m.delete()
