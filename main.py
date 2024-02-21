f97c231e7ad07cfe22f21308c76b4ea60460d1e6
chantroi <tonglanhchua@gmail.com>
2024-02-21 15:05:53 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import os, uvloop
 uvloop.install()
 from hydrogram import Client, filters, idle
-if os.getenv("SECRET"):
-    from lib.env import tokens
-    bot = Client("writer",
+from lib.env import tokens
+
+bot = Client("writer",
              tokens()[0],
              tokens()[1],
              bot_token=tokens()[2],
              plugins={"root": "plugins"})
 
-else:
-    bot = Client("writer",
-             plugins={"root": "plugins"})
-             
 bot.start()
 if os.path.exists("reset.txt"):
     with open("reset.txt", "r") as f:
