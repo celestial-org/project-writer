295629583b7fc69b8a66be756b618908f002df2e
chantroi <tonglanhchua@gmail.com>
2024-02-26 17:59:46 +0700

    update plugins/Others/TrackLink.py

--- plugins/Others/TrackLink.py
+++ plugins/Others/TrackLink.py
@@ -18,7 +18,11 @@ def track_link(c, m):
         req = [part.replace('...', '')  for part in m.command if any(part in  ['headers', 'body', 'picture'])][0]
     else:
         req = "headers"
-    for url in [url for url in m.text.split(None) if any(scheme in url for scheme in ["http://", "https://"])]:
+    if any(sche in m.text for sche in ['http://', 'https://']):
+        text = m.text
+    elif any(sche in m.reply_to_message.text for sche in ['http://', 'https://']):
+        text = m.reply_to_message.text
+    for url in [url for url in text.split(None) if any(scheme in url for scheme in ["http://", "https://"])]:
         r = requests.request(method, url, headers={"User-Agent": "Writer/1"}, proxies={"http": "http://127.0.0.1:8888", "https": "http://127.0.0.1:8888"})
         headers = "\n".join([f"{k.upper()}: {v}" for k, v in r.headers.items()])
         m.reply(f"```json\n{headers}```", quote=True)
