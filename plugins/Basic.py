21d98fda643fb248a0d2909948546dbe8f204787
chantroi <tonglanhchua@gmail.com>
2024-03-17 17:40:21 +0700

    update plugins/Basic.py

--- plugins/Basic.py
+++ plugins/Basic.py
@@ -5,7 +5,7 @@ from lib.env import config_tool
 @Client.on_message(filters.command("helps"))
 def help_list(c, m):
     m.reply_chat_action(ChatAction.TYPING)
-    with open('text/helps.txt') as f:
+    with open('plugins/text/helps.txt') as f:
         text = f.read()
     text = text.replace("{first_name}", m.from_user.first_name)
     text = text.replace("{uid}", str(m.from_user.id))
