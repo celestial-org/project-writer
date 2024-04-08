91e982e31a2fe64ff51a0f00db6b1ebb694511f1
chantroi <tonglanhchua@gmail.com>
2024-04-08 07:41:41 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import os
 import uvloop
 uvloop.install()
-from hydrogram import Client, idle
-from environment import api_id, api_hash, bot_token
 
-bot = Client("writer",
-             api_id,
-             api_hash,
-             bot_token=bot_token,
-             plugins= dict(root='src')
-            )
-
-
-bot.start()
-if os.path.exists("reset.txt"):
-    with open("reset.txt", "r") as f:
-        bot.send_message(int(f.read()),
-                                        "Chương trình đã được khởi động")
-    os.remove("reset.txt")
-print("V2Writer", flush=True)
-os.system("chmod +x ./lite")
-idle()
+if __name__ == "__main__":
+    from hydrogram import Client, idle
+    from environment import api_id, api_hash, bot_token
+    
+    bot = Client("writer",
+                 api_id,
+                 api_hash,
+                 bot_token=bot_token,
+                 plugins= dict(root='src')
+                )
+    
+    
+    bot.start()
+    if os.path.exists("reset.txt"):
+        with open("reset.txt", "r") as f:
+            bot.send_message(int(f.read()),
+                                            "Chương trình đã được khởi động")
+        os.remove("reset.txt")
+    print("V2Writer", flush=True)
+    os.system("chmod +x ./lite")
+    idle()
