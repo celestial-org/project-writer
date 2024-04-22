2f475ee0ac5677140c3a8e61d7b1570f8d09c573
chantroi <tonglanhchua@gmail.com>
2024-04-22 02:40:40 +0000

    update to async

--- main.py
+++ main.py
@@ -7,17 +7,16 @@ if __name__ == "__main__":
     from hydrogram import Client, idle
     from environment import api_id, api_hash, bot_token
 
-    bot = Client("writer",
-                 api_id,
-                 api_hash,
-                 bot_token=bot_token,
-                 plugins=dict(root='plugins'))
+    bot = Client(
+        "writer", api_id, api_hash, bot_token=bot_token, plugins=dict(root="plugins")
+    )
 
-    bot.start()
-    if os.path.exists("reset.txt"):
-        with open("reset.txt", "r") as f:
-            bot.send_message(int(f.read()), "Chương trình đã được khởi động")
-        os.remove("reset.txt")
-    print("V2Writer", flush=True)
-    os.system("chmod +x ./lite")
-    idle()
+    async def main():
+        await bot.start()
+        if os.path.exists("reset.txt"):
+            with open("reset.txt", "r") as f:
+                await bot.send_message(int(f.read()), "Chương trình đã được khởi động")
+            os.remove("reset.txt")
+        print("V2Writer", flush=True)
+        os.system("chmod +x ./lite")
+        await idle()
