5cd82bc6307bee5c43bb391ea9b9c0179bcdb1ee
vscode <vscode@vscode.dev>
2024-04-28 12:15:15 +0000

    update

--- plugins/writing_commands/slash_get.py
+++ plugins/writing_commands/slash_get.py
@@ -9,9 +9,9 @@ def get_urls(c, m):
     user_id = m.from_user.id
     filename = f"{user_id}"
     text = [
-        f'**Shared Link:**{v2tool}/get/share',
-        f'**Private Link:**{v2tool}/get/{filename}',
-        f'**Update Link:**{v2tool}/update/{filename}',
+        f'**Shared Link:** {v2tool}/get/share',
+        f'**Private Link:** {v2tool}/get/{filename}',
+        f'**Update Link:** {v2tool}/update/{filename}',
         'Sử dụng **Update Link** để cập nhật lại các server trong **Private Link**. Có thể dùng trực tiếp **Update Link** nhưng sẽ chậm hơn do phải truy cập đến từng subscription.',
         '**Shared Link** sẽ tự động cập nhật sau mỗi giờ. Không khuyến khích sử đụng link update của **Shared Link**'
     ]
