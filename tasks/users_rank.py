a77e45b2c8c52bf307e4f26a3357006ecc647755
GColab <tonglanhchua@gmail.com>
2024-05-03 08:31:16 +0000

    ...

--- tasks/users_rank.py
+++ tasks/users_rank.py
@@ -15,21 +15,17 @@ bot_token = res["bot"]["wb_tg"]
 
 
 app = Client("rank_counter", api_id, api_hash, bot_token=bot_token)
-conn = psycopg2.connect(neon_url)
-cursor = conn.cursor()
-cursor.execute(
-    """
-    CREATE TABLE IF NOT EXISTS user_ranks (
-        user_key VARCHAR(255) PRIMARY KEY,
-        count INTEGER
-    )
-"""
-)
-conn.commit()
+
+
+def neon():
+    conn = psycopg2.connect(neon_url)
+    cursor = conn.cursor()
+    return conn, cursor
 
 
 @app.on_message(filters.chat("share_v2ray_file"), group=2)
 def counter(c, m):
+    conn, cursor = neon()
     if m.from_user:
         key = f"[{m.from_user.first_name}](tg://user?id={m.from_user.id})"
         cursor.execute("SELECT count FROM user_ranks WHERE user_key = %s", (key,))
@@ -45,10 +41,12 @@ def counter(c, m):
                 "INSERT INTO user_ranks (user_key, count) VALUES (%s, %s)", (key, count)
             )
         conn.commit()
+        conn.close()
 
 
 @app.on_message(filters.command("rank"))
 def get_rank(c, m):
+    conn, cursor = neon()
     m.reply_chat_action(ChatAction.TYPING)
     cursor.execute(
         "SELECT user_key, count FROM user_ranks ORDER BY count DESC LIMIT 10"
@@ -64,6 +62,7 @@ def get_rank(c, m):
     ]
     text = "Share V2ray Group Ranking:\n\n\n" + "\n".join(users)
     m.reply(text, quote=True)
+    conn.close()
 
 
 print("Ranking bot is running")
