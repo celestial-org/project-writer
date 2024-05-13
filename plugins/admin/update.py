26b36dfd779a81421ab05a7178c204e5d5965eee
dash <tonglanhchua@gmail.com>
2024-05-13 08:43:22 +0000

    update PythonImportHelper-v2-Completion.json and update.py

--- plugins/admin/update.py
+++ plugins/admin/update.py
@@ -1,9 +1,7 @@
 import os
 import sys
-
 from hydrogram import Client, filters
 from hydrogram.enums import ChatAction
-
 from .admin import admin
 
 
@@ -11,9 +9,9 @@ from .admin import admin
 def update_server(c, m):
     m.reply_chat_action(ChatAction.TYPING)
     m.reply("Đang cập nhật hệ thống...")
-    os.system('git config --global user.name "Writer"')
-    os.system('git config --global user.email "duongchantroi@alwaysdata.net"')
-    os.system("git config --global pull.rebase true")
+    # os.system("git config --global user.name Writer")
+    # os.system("git config --global user.email bosuutap@alwaysdata.net")
+    # os.system("git config --global pull.rebase true")
     os.system("git add *")
     os.system('git commit -a -m "UPDATE"')
     os.system("git pull")
