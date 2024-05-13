e5f35b9061cec890791ae56beafb50dea6707c2f
dash <tonglanhchua@gmail.com>
2024-05-13 08:50:34 +0000

    update PythonImportHelper-v2-Completion.json and reset.py

--- plugins/admin/reset.py
+++ plugins/admin/reset.py
@@ -3,14 +3,13 @@ import sys
 
 from hydrogram import Client, filters
 from hydrogram.enums import ChatAction
-
 from .admin import admin
 
 
 @Client.on_message(filters.command("reset") & filters.user(admin))
 def reset_program(c, m):
     m.reply_chat_action(ChatAction.TYPING)
-    m.reply("Đang khởi động lại chương trình Client...")
+    md = m.reply("Đang khởi động lại chương trình Client...")
     with open("reset.txt", "w") as f:
-        f.write(str(m.chat.id))
+        f.write(str(m.chat.id) + ":" + str(md.id))
     os.execl(sys.executable, sys.executable, *sys.argv)
