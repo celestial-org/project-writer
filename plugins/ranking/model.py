3aafb366d38e10df7c530d797c15c62f036dde7b
gcolab <bosuutap@alwaysdata.net>
2024-05-16 14:00:32 +0000

    chore: update 1 file and delete 1 file

---
+++ plugins/ranking/model.py
@@ -0,0 +1,114 @@
+import re
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "rank"
+    user_id = Column(Integer, primary_key=True)
+    first_name = Column(String(500))
+    last_name = Column(String(500), nullable=True)
+    username = Column(String(500), nullable=True)
+    exp = Column(Integer, default=0)
+
+
+class DB:
+    def __init__(self, db_url):
+        self.engine = create_engine(db_url)
+        Base.metadata.create_all(self.engine)
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
+
+    def update(
+        self, user_id: int, first_name: str, last_name: str, username: str, exp: int
+    ):
+        user = self.session.query(User).filter(User.user_id == user_id).first()
+        if user:
+            exp = exp + user.exp
+        user = User(
+            user_id=user_id,
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            exp=exp,
+        )
+        self.session.merge(user)
+        self.session.commit()
+
+    def get(self, user_id):
+        return self.session.query(User).filter_by(user_id=user_id).first()
+
+    def list(self):
+        return self.session.query(User).all()
+
+
+def get_level(exp):
+    level = 0
+    require_exp = 2
+    while exp >= require_exp:
+        exp -= require_exp
+        level += 1
+        if level % 10 == 0:
+            require_exp *= 1.2
+
+    remaining_exp_for_next_level = require_exp - exp
+    return level, remaining_exp_for_next_level
+
+
+def ranks_prettier(user_rows):
+    ranks = []
+    emojis = ["🏆", "🎖️", "🏅", "🥇", "🥈", "🥉"]
+    sorted_users = sorted(user_rows, key=lambda x: x[1], reverse=True)
+    for i, row in enumerate(sorted_users[:20]):
+        rank = f"{emojis[i]}" if i < len(emojis) else f"{i + 1}"
+        user_info = [
+            f"**{rank}) {row[0]}**",
+            f"(level {get_level(row[1])})",
+        ]
+        ranks.append("  ".join(user_info))
+
+    return ranks
+
+
+def count_exp(m, level: int):
+    exp = 1
+    if m.text:
+        exp = len(m.text)
+        if m.text.startswith("/share"):
+            if any(scheme in m.text for scheme in ["http://", "https://"]):
+                exp += exp * 2
+    elif m.video or m.audio or m.file:
+        exp += 500
+    elif m.photo:
+        exp += 300
+    elif m.sticker:
+        exp += 50
+    if m.caption:
+        exp += len(m.caption)
+    if m.from_user.id == 6580709427:
+        exp += 100
+    return exp + level
+
+
+def get_user_rank(user_rows, target_user_id):
+    sorted_users = sorted(user_rows, key=lambda x: x[1], reverse=True)
+    for i, (user_id, _) in enumerate(sorted_users, start=1):
+        if user_id == target_user_id:
+            return i
+    return None
+
+def get_title(level):
+    if level < 50:
+        return
+    elif level < 200:
+        return
+    elif level < 600:
+        return
+    elif level < 2000:
+        return
+    elif level < 10000:
+        return
+    else:
+        return
---
+++ plugins/ranking/users_rank.py
@@ -0,0 +1,89 @@
+import os
+from hydrogram import Client, filters
+from hydrogram.enums import ChatAction
+from .model import (
+    DB,
+    User,
+    count_exp,
+    get_level,
+    ranks_prettier,
+    get_title,
+    get_user_rank,
+)
+
+
+@Client.on_message(filters.chat("share_v2ray_file"), group=2)
+def counter(c, m):
+    if m.from_user:
+        db = DB()
+        user_id = m.from_user.id
+        first_name = m.from_user.first_name
+        last_name = m.from_user.last_name
+        username = m.from_user.username
+        exp = count_exp(m)
+        db.update(user_id, first_name, last_name, username, exp)
+
+
+@Client.on_message(filters.command("rank"))
+def get_rank(c, m):
+    m.reply_chat_action(ChatAction.TYPING)
+    db = DB()
+    result_list = db.list()
+    users = []
+    for item in result_list:
+        if item.last_name:
+            name = item.first_name + " " + item.last_name
+        else:
+            name = item.first_name
+        if item.username:
+            user = f'<a href="https://{item.username}.t.me">{name}</a>'
+        else:
+            user = f'<a href="tg://user?id={item.user_id}">{name}</a>'
+        exp = item.exp
+        users.append((user, exp))
+    ranks = ranks_prettier(users)
+    text = ["**Bảng Phong Thần**", "\n\n".join(ranks)]
+    text = "\n\n\n".join(text)
+    m.reply(text, quote=True)
+
+
+@Client.on_message(filters.command("level"))
+def check_user_level(c, m):
+    m.reply_chat_action(ChatAction.TYPING)
+    db = DB()
+    if m.reply_to_message:
+        if m.reply_to_message.from_user:
+            user_id = m.reply_to_message.from_user.id
+    elif len(m.command) > 1:
+        username = m.command[1]
+        try:
+            user_id = c.get_users(username).id
+        except Exception as e:
+            print(e)
+            user_id = c.get_me().id
+    elif m.from_user:
+        user_id = m.from_user.id
+    else:
+        user_id = c.get_me().id
+    user = db.get(user_id)
+    exp = user.exp
+    if user.last_name:
+        name = user.first_name + " " + user.last_name
+    else:
+        name = user.first_name
+    if user.username:
+        user = f'<a href="https://{user.username}.t.me">{name}</a>'
+    else:
+        user = f'<a href="tg://user?id={user_id}">{name}</a>'
+    result_list = db.list()
+    rows = []
+    for row in result_list:
+        rows.append((row.user_id, row.exp))
+    level, needed_exp = get_level(exp)
+    rank = get_user_rank(rows, user_id)
+    title = get_title(level)
+    text = [
+        f"<b>{user}</b>",
+        f"Level: {level}",
+        f"Danh: {title}",
+    ]
