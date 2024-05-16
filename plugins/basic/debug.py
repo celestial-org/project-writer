b8c8fa55a5a994c134365652549e84ab11104ed7
gcolab <bosuutap@alwaysdata.net>
2024-05-16 08:17:05 +0000

    update PythonImportHelper-v2-Completion.json and debug.py

--- plugins/basic/debug.py
+++ plugins/basic/debug.py
@@ -4,3 +4,10 @@ from hydrogram import Client, filters
 @Client.on_message(filters.command("debug"))
 def debugger(c, m):
     m.reply(f"```json\n{m}```")
+
+
+@Client.on_message(filters.command("parse_mode"))
+def test_parse_mode(c, m):
+    text = m.text.replace(m.command[0], "")
+    m.reply(text)
+    m.delete()
