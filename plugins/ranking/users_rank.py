a63c2a29b4e924322305f5d496353df0190db59d
chantroi <tonglanhchua@gmail.com>
2024-05-17 01:03:30 +0700

    Update users_rank.py

--- plugins/ranking/users_rank.py
+++ plugins/ranking/users_rank.py
@@ -96,7 +96,7 @@ def check_user_level(c, m):
         f"<b>Thứ hạng:</b> `{rank}`",
         f"<b>EXP:</b> `{exp}xp`",
         f"<b>EXP cần để đạt <i>cấp {level + 1}</i>:</b> `{needed_exp}xp`",
-        f"<b>Đánh giá:</b> ```\n{title}```",
+        f"<b>Cảnh Giới:</b> ```\n{title}```",
         "**Lưu ý:**\nHệ thống tính điểm dựa trên một loạt các quy tắc phức tạp cho nên việc spam lượng lớn tin nhắn không giúp tăng nhiều điểm kinh nghiệm."
     ]
     text = "\n\n".join(text)
