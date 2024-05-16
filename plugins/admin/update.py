3398192edae21df30992576e42466d85181413f7
gcolab <bosuutap@alwaysdata.net>
2024-05-16 08:18:01 +0000

    update PythonImportHelper-v2-Completion.json and update.py

--- plugins/admin/update.py
+++ plugins/admin/update.py
@@ -9,11 +9,7 @@ from .admin import admin
 def update_server(c, m):
     m.reply_chat_action(ChatAction.TYPING)
     mp = m.reply("Đang cập nhật hệ thống...")
-    # os.system("git config --global user.name Writer")
-    # os.system("git config --global user.email bosuutap@alwaysdata.net")
-    # os.system("git config --global pull.rebase true")
-    os.system("git add *")
-    os.system('git commit -a -m "UPDATE"')
+    os.system("git stash")
     os.system("git pull")
     md = m.reply("Đã cập nhật xong đang khởi động lại...")
     mp.delete()
