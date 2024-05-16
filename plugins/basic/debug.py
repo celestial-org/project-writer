e7bf34aeefcc6d6d0a1e056d17be702bdaaf2741
gcolab <bosuutap@alwaysdata.net>
2024-05-16 08:21:33 +0000

    update PythonImportHelper-v2-Completion.json and debug.py

--- plugins/basic/debug.py
+++ plugins/basic/debug.py
@@ -6,7 +6,7 @@ def debugger(c, m):
     m.reply(f"```json\n{m}```")
 
 
-@Client.on_message(filters.command("parse_mode"))
+@Client.on_message(filters.command("parse"))
 def test_parse_mode(c, m):
     text = m.text.split(" ", 1)[1]
     m.reply(text)
