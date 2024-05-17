05c1714ae4920ca8f9ff6c7566e8d5a4a4a7aef7
gcolab <bosuutap@alwaysdata.net>
2024-05-17 10:57:01 +0000

    update PythonImportHelper-v2-Completion.json, users_rank.py and schedule.py

--- schedule.py
+++ schedule.py
@@ -15,7 +15,7 @@ def schedule(c):
                 name = item.first_name + " " + item.last_name
             else:
                 name = item.first_name
-            user = f"[{name}](tg://user?id={item.user_id})"
+            user = f"<a href='tg://user?id={item.user_id}'>{name}</a>"
             exp = item.exp
             users.append((user, exp))
         ranks = ranks_prettier(users)
