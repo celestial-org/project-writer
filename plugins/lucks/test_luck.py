0243be4153fcf13d863fa25153c025fd53b876a8
gcolab <bosuutap@alwaysdata.net>
2024-05-18 13:53:59 +0000

    chore: update PythonImportHelper-v2-Completion.json

---
+++ plugins/lucks/test_luck.py
@@ -0,0 +1,33 @@
+import random
+from datetime import datetime, timedelta
+from hydrogram import Client, filters
+from hydrogram.enums import ChatAction
+from hydrogram.types import ChatPermissions
+
+
+@Client.on_message(filters.command("roll"))
+def roll_luck(c, m):
+    m.reply_chat_action(ChatAction.TYPING)
+    c.send_dice(m.chat.id)
+    rand = random.randint(0, 1000)
+    choice = random.choice([rand, rand, rand, 0000, rand, rand])
+    if choice == 0000:
+        mtime = random.randint(0, 300)
+        m.reply(
+            f"Chúc mừng **{m.from_user.first_name}** nhận được khoá trò chuyện `{mtime}` phút🎉",
+            quote=True,
+        )
+        now = datetime.now()
+        delta = timedelta(minutes=mtime)
+        targ = now + delta
+        m.chat.restrict_member(
+            m.from_user.id, ChatPermissions(can_send_messages=False), until_date=targ
+        )
+    else:
+        m.reply(
+            f"Chúc mừng **{m.from_user.first_name}** nhận được `{choice}` điểm may mắn🎉",
+            quote=True,
+        )
+
+
+b
