fda51d10af1f2a63ae7a1273091c47debb4c8d9c
chantroi <tonglanhchua@gmail.com>
2024-05-19 02:00:35 +0000

    Update plugins

--- plugins/dice/test_luck.py
+++ plugins/dice/test_luck.py
@@ -8,7 +8,7 @@ from hydrogram.types import ChatPermissions
 def punish(m):
     mtime = random.randint(0, 300)
     m.reply(
-        f"**{m.from_user.first_name}** nhận được `{m.dice.value}` điểm, quá đen đủi cho một con người, hãy dành `{mtime}` phút để suy nghĩ lại về cuộc đời.",
+        f"**{m.from_user.first_name}** nhận được `{m.dice.value}` điểm, quá đen đủi cho ngày hôm nay. Hãy dành `{mtime}` phút cuộc đời để suy nghĩ về số phận.",
         quote=True,
     )
     now = datetime.now()
@@ -26,26 +26,41 @@ def roll_dice(c, m):
     m.reply_chat_action(ChatAction.TYPING)
     dice = m.dice
     if dice.emoji == "🎰":
-        if dice.value > 19:
+        if dice.value > 59:
             m.reply(
-                f"Chúc mừng **{m.from_user.first_name}** nhận được `{dice.value}` điểm🎉",
+                f"Chúc mừng **{m.from_user.first_name}** nhận được `{dice.value}` điểm. Hôm nay bạn thật may mắn🎉",
+                quote=True,
+            )
+        elif dice.value > 19:
+            m.reply(
+                f"**{m.from_user.first_name}** nhận được `{dice.value}` điểm.",
                 quote=True,
             )
         else:
             punish(m)
     elif dice.emoji in ["🎳", "🎯", "🎲"]:
 
-        if dice.value > 2:
+        if dice.value == 6:
             m.reply(
-                f"Chúc mừng **{m.from_user.first_name}** nhận được `{dice.value}` điểm🎉",
+                f"Chúc mừng **{m.from_user.first_name}** nhận được `{dice.value}` điểm. Hôm nay bạn thật may mắn🎉",
+                quote=True,
+            )
+        elif dice.value > 2:
+            m.reply(
+                f"**{m.from_user.first_name}** nhận được `{dice.value}` điểm.",
                 quote=True,
             )
         else:
             punish(m)
     else:
-        if dice.value > 1:
+        if dice.value == 5:
             m.reply(
-                f"Chúc mừng **{m.from_user.first_name}** nhận được `{dice.value}` điểm🎉",
+                f"Chúc mừng **{m.from_user.first_name}** nhận được `{dice.value}` điểm. Hôm nay bạn thật may mắn🎉",
+                quote=True,
+            )
+        elif dice.value > 1:
+            m.reply(
+                f"**{m.from_user.first_name}** nhận được `{dice.value}` điểm.",
                 quote=True,
             )
         else:
