80201847ba8728a570127ae41eee6987e44e83ba
gcolab <bosuutap@alwaysdata.net>
2024-05-21 03:50:23 +0000

    update 2 files and delete 1 file

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import os
-from schedule import schedule
-
 
 if __name__ == "__main__":
     from hydrogram import Client, idle
     from environment import api_id, api_hash, bot_token
-    from schedule import schedule
 
     bot = Client(
         "writer", api_id, api_hash, bot_token=bot_token, plugins=dict(root="plugins")
     )
     bot.start()
-    schedule(bot)
     if os.path.exists("reset.txt"):
         with open("reset.txt", "r") as f:
             chat_id, m_id = f.read().split(":")
