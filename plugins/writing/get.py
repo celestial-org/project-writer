c094fdb6343db36b98328d8e70699d7e9ac3886f
chantroi <duongchantroi@alwaysdata.net>
2024-06-02 23:37:35 +0000

    update PythonImportHelper-v2-Completion.json and get.py

--- plugins/writing/get.py
+++ plugins/writing/get.py
@@ -8,12 +8,9 @@ v2tool = os.getenv("V2TOOL")
 @Client.on_message(filters.command("get"))
 def get_urls(c, m):
     m.reply_chat_action(ChatAction.TYPING)
-    user_id = m.from_user.id
-    filename = f"{user_id}"
     text = [
         f"**Shared Link:** {v2tool}/get/share",
-        "Các server trong Link sẽ tự động được làm mới sau mỗi lần truy cập, sau mỗi giờ."
-        f"Nếu phần mềm client của bạn không hỗ trợ cấu hình v2ray, hãy sử dụng công cụ để chuyển đổi: https://convert.v2ray-subscribe.workers.dev/?url={v2tool}/get/share&flag=sing-box\nThay `sing-box` bằng tên phần mềm client của bạn",
+        "Các server sẽ tự động được làm mới.",
     ]
     m.reply(
         "\n\n".join(text),
