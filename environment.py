d0084a4e949fed0e919a86fc0fe6da733cc24999
chantroi <duongchantroi@alwaysdata.net>
2024-06-04 11:49:06 +0000

    update PythonImportHelper-v2-Completion.json and environment.py

--- environment.py
+++ environment.py
@@ -2,18 +2,11 @@ import os
 import requests
 
 
-res = requests.get(os.getenv("SECRET")).json()
-
+res = requests.get(os.getenv("SECRET"), timeout=99).json()
 os.environ["V2TOOL"] = res["api"]["v2tool"]
-
 os.environ["TEST_SERVER"] = res["api"]["test"]
-
-os.environ["DETA_KEY"] = res["key"]["collection"]
-
-os.environ["TURSO_URL"] = res["data"]["turso"]
-
-api_id = res["key"]["api_id"]
-
-api_hash = res["key"]["api_hash"]
-
-bot_token = res["bot"]["wb_tg"]
+os.environ["DETA_KEY"] = res["deta"]["collection"]
+os.environ["TURSO_URL"] = res["sqlite"]["turso"]
+api_id = res["telegram"]["api_id"]
+api_hash = res["telegram"]["api_hash"]
+bot_token = res["bot"]["writer"]
