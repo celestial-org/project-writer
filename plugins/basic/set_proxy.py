c7ae67c1581de444af6f185b2eae7b0d30c80093
chantroi <duongchantroi@alwaysdata.net>
2024-06-08 11:22:52 +0000

    update PythonImportHelper-v2-Completion.json and set_proxy.py

--- plugins/basic/set_proxy.py
+++ plugins/basic/set_proxy.py
@@ -6,7 +6,7 @@ from hydrogram import Client, filters
 @Client.on_message(filters.command("set_proxy") & filters.user(5665225938))
 def set_proxy(c, m):
     proxy = m.command[1]
-    os.system("killall -9 lite")
+    os.system("pkill -9 lite")
     os.system(f"./lite -p 6868 {proxy} &")
     stt = m.reply("Đã thiết lập proxy")
     m.delete()
