faf2ba5d8cc2c1d2d6307f4bd89fb8e3b3a3686d
chantroi <duongchantroi@alwaysdata.net>
2024-06-08 11:35:04 +0000

    update PythonImportHelper-v2-Completion.json, Dockerfile, set_proxy.py and start.sh

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM python
 
 RUN useradd -m -u 1000 user
-WORKDIR /home/user
-RUN git clone https://github.com/chantroi/project-writer bot
+
 WORKDIR /home/user/bot
-RUN pip install -r requirements.txt
+
 RUN chown -R user:user /home/user/bot
-RUN chmod +x ./start.sh && chmod +x ./lite
 
 USER user
+
 EXPOSE 8080
+
 ENTRYPOINT ["./start.sh"]
--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-export PORT=${PORT:-8080}
+git clone https://github.com/chantroi/project-writer .
+
+chmod +x ./start.sh && chmod +x ./lite
+
+pip install -r requirements.txt
+
+export PORT=${PORT:-8080} http_proxy=http://127.0.0.1:6868 https_proxy=http://127.0.0.1:6868
+
 gunicorn -b 0.0.0.0:$PORT helloworld:app &
 python main.py
