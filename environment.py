d4596bcf9741b54716831d2d88e5cd75c3460547
chantroi <duongchantroi@alwaysdata.net>
2024-06-15 11:42:56 +0000

    update PythonImportHelper-v2-Completion.json and environment.py

--- environment.py
+++ environment.py
@@ -5,5 +5,5 @@ import requests
 res = requests.get(os.getenv("SECRET"), timeout=99).json()
 os.environ["V2TOOL"] = res["api"]["v2tool"]
 os.environ["TEST_SERVER"] = res["api"]["test"]
-os.environ["TURSO_URL"] = res["database"]["notes"]
+os.environ["DB_URL"] = res["database"]["notes"]
 bot_token = res["bot"]["writer"]
