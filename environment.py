7ecf84b5fa37b5034dda102d40c5459edc9cb823
Chan Troi Moi <tonglanhchua@gmail.com>
2024-08-13 08:51:10 +0000

    update environment.py

--- environment.py
+++ environment.py
@@ -5,7 +5,7 @@ import requests
 res = requests.get(os.getenv("SECRET"), timeout=99).json()
 os.environ["V3TOOL"] = res["api"]["v3tool"]
 os.environ["TEST_SERVER"] = res["api"]["test"]
-db_url = res["db"]["libsql"][1]
+db_url = res["db"]["libsql"]
 bot_token = res["access"]["telegram"]["writer"]
 reverse_link = res["api"]["reverse"]
 deta_key = res["access"]["deta"]["sm"]
