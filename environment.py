9e6506bf6e89503b18859a2dcfb3fee94f38d147
Chan Troi Moi <tonglanhchua@gmail.com>
2024-08-13 11:07:24 +0000

    update environment.py and get.py

--- environment.py
+++ environment.py
@@ -3,7 +3,7 @@ import requests
 
 
 res = requests.get(os.getenv("SECRET"), timeout=99).json()
-os.environ["V3TOOL"] = res["api"]["v3tool"]
+os.environ["v2tool"] = res["api"]["v2tool"]
 os.environ["TEST_SERVER"] = res["api"]["test"]
 db_url = res["db"]["libsql"]
 bot_token = res["access"]["telegram"]["writer"]
--- plugins/writing/get.py
+++ plugins/writing/get.py
@@ -2,15 +2,15 @@ import os
 from pyrogram import Client, filters
 from pyrogram.enums import ChatAction
 
-v3tool = os.getenv("V3TOOL")
+v2tool = os.getenv("v2tool")
 
 
 @Client.on_message(filters.command("get"))
 async def get_urls(c, m):
     await m.reply_chat_action(ChatAction.TYPING)
     text = [
-        f"**Shared Link:** {v3tool}",
-        f"**Misc Link:** {v3tool}/misc",
+        f"**Shared Link:** {v2tool}/get/default",
+        f"**Misc Link:** {v2tool}/get/misc",
         "Các server sẽ tự động được làm mới.",
     ]
     await m.reply(
