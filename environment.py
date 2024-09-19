cb639d62e715225f5f6ee029ba64576312f205bb
Chan Troi Moi <tonglanhchua@gmail.com>
2024-09-19 01:31:04 +0000

    update environment.py

--- environment.py
+++ environment.py
@@ -5,5 +5,5 @@ import requests
 res = requests.get(os.getenv("SECRET"), timeout=99).json()
 os.environ["v2tool"] = res["api"]["v2tool"]
 os.environ["TEST_SERVER"] = res["api"]["test"]
-db_url = res["db"]["libsql"]
-bot_token = res["access"]["telegram"]["writer"]
+db_url = res["database"]["libsql"]
+bot_token = res["telegram"]["bot"]["writer"]
