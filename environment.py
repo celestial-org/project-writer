80c95499fd78dbceefb437d4acebd6e53d58cb90
CodeSandbox <tonglanhchua@gmail.com>
2025-02-18 01:31:11 +0000

    Remove environment configuration and sensitive information from environment.py

--- environment.py
+++
@@ -1,8 +0,0 @@
-import os
-import requests
-
-
-os.environ["v2tool"] = "https://v2tool.flaring.workers.dev"
-os.environ["TEST_SERVER"] ="https://google.com"
-db_url = "sqlite+libsql://default-chantroi.turso.io/?authToken=eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MzEyMDc4NjEsImlkIjoiZWZmNDFmM2MtMTVkOS00ZGUwLWJjMWUtOTgzMWVjZmFjZDgxIn0.u0pH_nhMIMiuwAuuiD0icTNsnODYPHWLDbYYyZx428Ed_zK36ovD_mQkKZrsilNeYPPwvsRmyGe-wcFQLjPkDw&secure=true"
-bot_token = "6941591623:AAFHUmAZjBPMkoB0hV4tS8VA0x6fdMFP6XI"
